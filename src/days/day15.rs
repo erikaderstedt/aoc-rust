2ac0e5f44492c8a115b0b638735d09431bd94c15
Erik Aderstedt <erik@aderstedtsoftware.com>
2020-12-15 08:13:29 +0100

    Day 15 refactored

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,56 +1,26 @@
 use crate::common::Solution;
-// use crate::common::parsed_from_each_line;
-// use regex::Regex;
-// use itertools::Itertools;
-use std::collections::HashMap;
 
-pub fn solve(input: &str) -> Solution {
-    let nums: [usize;6] = [1,0,15,2,10,13];
-//    let nums: [usize;3] = [0,3,6];
+fn determine_nth_number_spoken(starting_nums: &[u32], n: u32) -> u32 {
+    let mut spoken = vec![0u32; n as usize];
+    for i in 0..(starting_nums.len()-1) {
+        spoken[starting_nums[i] as usize] = (i+1)  as u32;
+    }
 
-    let p1 = {
-        let mut spoken: HashMap<usize,usize> = nums.iter().cloned().enumerate().map(|(i,v)| (v,i+1)).collect();
+    ((starting_nums.len())..(n as usize)).fold(starting_nums[starting_nums.len()-1], |last_spoken, turn_number| {
+        let speak = match spoken[last_spoken as usize] {
+            0 => 0,
+            n => (turn_number as u32) - n,
+        };
+        spoken[last_spoken as usize] = turn_number as u32;
+        speak
+    })
+}
 
-        let mut last_was_previously_spoken: Option<usize> = None; 
-        let mut p1 = 0;
-        
-        for turn_number in (nums.len()+1)..=2020 {
-            let new_number = match last_was_previously_spoken {
-                None => 0,
-                Some(v) => v, 
-            };
-            last_was_previously_spoken = match spoken.get(&new_number) {
-                Some(previous_turn) => Some(turn_number - previous_turn),
-                None => None,
-            };
-    //        println!("{} {} {:?}",new_number, turn_number, last_was_previously_spoken);
-            spoken.insert(new_number, turn_number);
-            p1 = new_number;
-        }
-        p1
-    };
+pub fn solve(_input: &str) -> Solution {
+    let nums: [u32;6] = [1,0,15,2,10,13];
 
-    let p2 = {
-        let mut spoken: HashMap<usize,usize> = nums.iter().cloned().enumerate().map(|(i,v)| (v,i+1)).collect();
-
-        let mut last_was_previously_spoken: Option<usize> = None; 
-        let mut p1 = 0;
-        
-        for turn_number in (nums.len()+1)..=30000000 {
-            let new_number = match last_was_previously_spoken {
-                None => 0,
-                Some(v) => v, 
-            };
-            last_was_previously_spoken = match spoken.get(&new_number) {
-                Some(previous_turn) => Some(turn_number - previous_turn),
-                None => None,
-            };
-    //        println!("{} {} {:?}",new_number, turn_number, last_was_previously_spoken);
-            spoken.insert(new_number, turn_number);
-            p1 = new_number;
-        }
-        p1
-    };
+    let p1 = determine_nth_number_spoken(&nums, 2020u32);
+    let p2 = determine_nth_number_spoken(&nums, 30000000u32);
 
     Solution { part_1: p1.to_string(), part_2: p2.to_string() }
 }
