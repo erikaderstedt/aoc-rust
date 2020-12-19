8aa6873befa8d35250d364aae7f8ac9c99c58e9f
Erik Aderstedt <erik@aderstedtsoftware.com>
2020-12-19 15:54:33 +0100

    Day 19 refactored

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -2,7 +2,6 @@ use crate::common::Solution;
 use itertools::Itertools;
 use std::collections::HashMap;
 
-
 enum Rule {
     Character(u8),
     Single(Vec<usize>),
@@ -25,60 +24,61 @@ impl std::str::FromStr for Rule {
     }
 }
 
-type RulesList = HashMap<usize,Rule>;
-
-impl Rule {
-
-    fn process_indices<'a>(rule_indices: &Vec<usize>, rules: &RulesList, s: &'a [u8]) -> Vec<&'a [u8]> {
-        rule_indices.iter().fold(vec![s], |acc, rule_index| {
-            acc.into_iter().map(|r| {
-                rules[rule_index].matches(rules, r)
-            }).flatten().collect()
-        })
-    }
-    // Returns the possible different remaining strings after matching this rule.
-    fn matches<'a>(&self, rules: &RulesList, s: &'a [u8]) -> Vec<&'a [u8]> {
-        match self {
-            Rule::Character(c) => match s.get(0) {
-                Some(c1) if c == c1 => { vec![&s[1..]] }
-                _ => vec![],
-            },
-            Rule::Single(rule_indices) => Rule::process_indices(rule_indices, rules, s),
-            Rule::Multiple((left, right)) => {
-                let mut r1 = Rule::process_indices(left, rules, s);
-                r1.extend(Rule::process_indices(right, rules, s));
-                r1
-            },
-        }
-    }
+struct RulesList {
+    rules: HashMap<usize,Rule>
 }
 
-fn number_of_matches_rule_0(messages: &Vec<&str>, rules: &RulesList) -> usize {
-    messages.iter()
-        .filter(|line| {
-            rules[&0].matches(&rules,&line.as_bytes()).iter().any(|v| v.len() == 0)
-        })
-        .count()
+impl RulesList {
+
+    fn match_sequence<'a>(&self, indices: &Vec<usize>, s: &'a [u8], q: &mut Vec<usize>) -> bool {
+        q.extend(indices.iter().rev());
+        self.match_on(s, q)
+    }
+
+    fn match_on<'a>(&self, s: &'a [u8], mut q: &mut Vec<usize>) -> bool {
+        match q.pop() {
+            Some(rule_index) => match &self.rules[&rule_index] {
+                Rule::Character(c1) => matches!(s.get(0), Some(c) if c == c1) && self.match_on(&s[1..], q),
+                Rule::Single(indices) => self.match_sequence(&indices, s, &mut q),
+                Rule::Multiple((left,right)) => {
+                    self.match_sequence(&left, s, &mut q.clone()) ||
+                    self.match_sequence(&right, s, &mut q.clone())
+                }
+            }
+            None => s.len() == 0,
+        }
+    }
+
+    fn matches_to_rule_0(&self, messages: &Vec<&str>) -> usize {
+        messages.iter()
+            .filter(|line| {
+                let mut q = vec![0];
+                self.match_on(line.as_bytes(), &mut q)
+            })
+            .count()
+    }
 }
 
 pub fn solve(input: &str) -> Solution {
-    let mut rules: RulesList = input.lines().take_while(|&line| line != "").map(|line| {
-        let (num, ch) = line.split(": ").collect_tuple().unwrap();
-        let num = num.parse::<usize>().unwrap();
-        let rule = ch.parse::<Rule>().unwrap();
-        (num, rule)
-    }).collect();
+    let mut matcher = RulesList {
+        rules:  input.lines().take_while(|&line| line != "").map(|line| {
+                    let (num, ch) = line.split(": ").collect_tuple().unwrap();
+                    let num = num.parse::<usize>().unwrap();
+                    let rule = ch.parse::<Rule>().unwrap();
+                    (num, rule)
+                }).collect(),
+    };
 
     let messages: Vec<&str> = input.lines()
         .skip_while(|&line| line != "")
         .skip(1).collect();
 
-    let p1 = number_of_matches_rule_0(&messages, &rules);
+    let p1 = matcher.matches_to_rule_0(&messages);
 
-    rules.insert(8, "42 | 42 8".parse::<Rule>().unwrap());
-    rules.insert(11, "42 31 | 42 11 31".parse::<Rule>().unwrap());
-
-    let p2 = number_of_matches_rule_0(&messages, &rules);
+    matcher.rules.insert(8, Rule::Multiple((vec![42], vec![42,8])));
+    matcher.rules.insert(11, Rule::Multiple((vec![42,31], vec![42,11,31])));
+    
+    let p2 = matcher.matches_to_rule_0(&messages);
 
     Solution { part_1: p1.to_string(), part_2: p2.to_string() }
 }
