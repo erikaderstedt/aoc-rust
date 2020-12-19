7ba2e740bc16b2e2b2f2116de4c0f70b11748cde
Erik Aderstedt <erik@aderstedtsoftware.com>
2020-12-19 12:45:01 +0100

    Day 19 refactored

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -2,45 +2,10 @@ use crate::common::Solution;
 use itertools::Itertools;
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq,Eq)]
 enum Rule {
-    Character(char),
+    Character(u8),
     Single(Vec<usize>),
     Multiple((Vec<usize>, Vec<usize>)),
-}
-
-type RulesList = HashMap<usize,Rule>;
-
-impl Rule {
-    // Returns the possible different remaining strings
-    fn matches<'a>(&self, rules: &RulesList, s: &'a str) -> Vec<&'a str> {
-        match self {
-            Rule::Character(c) => match s.chars().next() {
-                Some(c1) if c1 == *c => vec![&s[1..]],
-                _ => vec![],
-            },
-            Rule::Single(inner) => {
-                let mut r = vec![s];
-                let mut r2;
-                for rule in inner {
-                    r2 = Vec::new();
-                    for r in &r {
-                        let mut next_results = rules[rule].matches(rules, r);
-                        r2.append(&mut next_results);
-                    }
-                    if r2.len() == 0 { return vec![]; }
-                    r = r2;
-                }
-                r
-            },
-            Rule::Multiple((left, right)) => {
-                let mut r1 = Rule::Single(left.clone()).matches(rules, s);
-                let r2 = Rule::Single(right.clone()).matches(rules, s);
-                r1.extend(r2);
-                r1
-            }
-        }
-    }
 }
 
 impl std::str::FromStr for Rule {
@@ -48,7 +13,7 @@ impl std::str::FromStr for Rule {
 
     fn from_str(ch: &str) -> std::result::Result<Rule,Self::Err> {
         Ok(if ch.contains("\"") {
-            Rule::Character(ch.chars().skip(1).next().unwrap())
+            Rule::Character(ch.as_bytes()[1])
         } else if ch.contains("|") {
             let (a,b) = ch.split('|').collect_tuple().unwrap();
             Rule::Multiple((a.split(' ').filter(|c| c.len() > 0).map(|c| c.parse::<usize>().unwrap()).collect(),
@@ -59,11 +24,46 @@ impl std::str::FromStr for Rule {
     }
 }
 
-fn number_of_matches_rule_0(input: &str, rules: &RulesList) -> usize {
-    input.lines().skip_while(|&line| line != "").skip(1).filter(|line| {
-        rules[&0].matches(&rules,line).iter().any(|v| v.len() == 0)
-    })
-    .count()
+type RulesList = HashMap<usize,Rule>;
+
+impl Rule {
+    // Returns the possible different remaining strings after matching this rule.
+    fn matches<'a>(&self, rules: &RulesList, s: &'a [u8]) -> Vec<&'a [u8]> {
+        match self {
+            Rule::Character(c) => match s.get(0) {
+                Some(c1) if c == c1 => { vec![&s[1..]] }
+                _ => vec![],
+            },
+            Rule::Single(rule_indices) => {
+                rule_indices.iter().fold(vec![s], |acc, rule_index| {
+                    acc.into_iter().map(|r| {
+                        rules[rule_index].matches(rules, r)
+                    }).flatten().collect()
+                })
+            },
+            Rule::Multiple((left, right)) => {
+                left.iter().fold(vec![s], |acc, rule_index| {
+                    acc.into_iter().map(|r| {
+                        rules[rule_index].matches(rules, r)
+                    }).flatten().collect()
+                }).into_iter().chain(
+                right.iter().fold(vec![s], |acc, rule_index| {
+                    acc.into_iter().map(|r| {
+                        rules[rule_index].matches(rules, r)
+                    }).flatten().collect()
+                }).into_iter())
+                .collect()
+            }
+        }
+    }
+}
+
+fn number_of_matches_rule_0(messages: &Vec<&str>, rules: &RulesList) -> usize {
+    messages.iter()
+        .filter(|line| {
+            rules[&0].matches(&rules,&line.as_bytes()).iter().any(|v| v.len() == 0)
+        })
+        .count()
 }
 
 pub fn solve(input: &str) -> Solution {
@@ -74,12 +74,16 @@ pub fn solve(input: &str) -> Solution {
         (num, rule)
     }).collect();
 
-    let p1 = number_of_matches_rule_0(input, &rules);
+    let messages: Vec<&str> = input.lines()
+        .skip_while(|&line| line != "")
+        .skip(1).collect();
+
+    let p1 = number_of_matches_rule_0(&messages, &rules);
 
     rules.insert(8, "42 | 42 8".parse::<Rule>().unwrap());
     rules.insert(11, "42 31 | 42 11 31".parse::<Rule>().unwrap());
 
-    let p2 = number_of_matches_rule_0(input, &rules);
+    let p2 = number_of_matches_rule_0(&messages, &rules);
 
     Solution { part_1: p1.to_string(), part_2: p2.to_string() }
 }
