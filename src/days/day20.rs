a10e8cb2ea19300f4013f636b054a3b9ed7fa19a
Erik Aderstedt <erik@aderstedtsoftware.com>
2020-12-20 10:11:53 +0100

    Day 20 pt 2

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -4,6 +4,7 @@ use pathfinding::prelude::bfs;
 use std::hash::{Hash, Hasher};
 
 type Tile = [[bool;10];10];
+type Image = Vec<Vec<bool>>;
 
 enum Edge { Top, Bottom,Left,Right }
 
@@ -158,6 +159,72 @@ impl State {
         }
     }
 
+    fn get_image(&self) -> Image {
+        let mut v: Image = Vec::new();
+        let mut i = 0;
+        for row in 0..self.side {
+            for row_in_tile in 1..=(TILE_SIDE - 2) {
+                v.push(Vec::new());
+                for column in 0..self.side {
+                    let placed: &PlacedTile = self.grid[column+row*self.side].as_ref().unwrap();
+                    v[i].extend(placed.tile[row_in_tile].iter().skip(1).take(TILE_SIDE-2))
+                }
+                i += 1;
+            }
+        }
+        v
+    }
+
+}
+
+fn rotate_grid(g: &Image) -> Image {
+    let s = g.len();
+    (0..s).map(|r| 
+        (0..s).map(|c| g[c][r]).collect()
+    ).collect()
+}
+
+fn flip_grid(g: &Image) -> Image {
+    g.iter().rev().cloned().collect()
+}
+
+const SEA_MONSTER_PIXELS: usize = 15;
+const SEA_MONSTER_WIDTH: usize = 20;
+const SEA_MONSTER_HEIGHT: usize = 3;
+
+fn num_sea_monsters_in_image(image: &Image) -> usize {
+    let s = image.len();
+    (0..(s - SEA_MONSTER_HEIGHT)).map(|r| 
+        (0..(s-SEA_MONSTER_WIDTH)).filter(|c| sea_monster_at(&r, c, image)).count()
+    ).sum::<usize>()
+}
+
+fn sea_monster_at(row: &usize, col: &usize, grid: &Image) -> bool {
+    row + 2 < grid.len() &&
+    col + 19 < grid[0].len() &&
+    // 01234567890123456789
+    //                   # 
+    // #    ##    ##    ###
+    //  #  #  #  #  #  #   
+    grid[row+1][col+0] &&
+    grid[row+2][col+1] &&
+    grid[row+2][col+4] &&
+    grid[row+1][col+5] &&
+    grid[row+1][col+6] &&
+    grid[row+2][col+7] &&
+    grid[row+2][col+10] &&
+    grid[row+1][col+11] &&
+    grid[row+1][col+12] &&
+    grid[row+2][col+13] &&
+    grid[row+2][col+16] &&
+    grid[row+1][col+17] &&
+    grid[row+0][col+18] &&
+    grid[row+1][col+18] &&
+    grid[row+1][col+19]
+}
+
+fn sea_roughness(grid:&Image, num_sea_monsters: &usize) -> usize {
+    grid.iter().map(|g| g.iter().filter(|&x| *x).count()).sum::<usize>() - *num_sea_monsters*SEA_MONSTER_PIXELS
 }
 
 pub fn solve(input: &str) -> Solution {
@@ -173,8 +240,31 @@ pub fn solve(input: &str) -> Solution {
         tiles.insert(c, [tile[0],tile[1],tile[2],tile[3],tile[4],tile[5],tile[6],tile[7],tile[8],tile[9],]);
     }
 
-    let p1_result = bfs(&State::from_tiles(tiles), State::successors, State::all_laid_out).expect("No solution pt 1");
-    let p1 = p1_result[p1_result.len()-1].corner_tiles_product();
+    let result = bfs(&State::from_tiles(tiles), State::successors, State::all_laid_out).expect("No solution pt 1");
+    let final_state = &result[result.len() - 1];
+
+    let p1 = final_state.corner_tiles_product();
+    println!("p1 {}", p1);
+    let image = final_state.get_image();
+//    println!("{:?}", image);
+    println!("got image {} {}", image.len(), image[0].len());
+    let i1 = rotate_grid(&image);
+    let i2 = rotate_grid(&i1);
+    let i3 = rotate_grid(&i2);
+    let f1 = flip_grid(&image);
+    let f2 = flip_grid(&i1);
+    let f3 = flip_grid(&i2);
+    let f4 = flip_grid(&i3);
+    let monsters: Vec<(usize,usize)> = 
+    [image, i1, i2, i3, f1, f2, f3, f4].into_iter().map(|image| -> (usize, usize) {
+        let n = num_sea_monsters_in_image(&image);
+        let r = sea_roughness(&image, &n);
+        (n, r)
+     }).collect();
+
+    println!("{:?}", monsters);
+    
+
     
     Solution { part_1: p1.to_string(), part_2: "".to_string() }
 }
