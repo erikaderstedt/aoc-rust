df3cca4b7142cdfeb1ba9ca17940c4c197c8f9bf
Erik Aderstedt <erik@aderstedtsoftware.com>
2020-12-21 17:06:46 +0100

    Day 21 refactored a bit more

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -7,50 +7,39 @@ struct Food<'a> {
     allergens: HashSet<&'a str>,
 }
 
-impl<'a> Food<'a> {
-    fn parse(s: &'a str) -> Food {
+pub fn solve(input: &str) -> Solution {
+    let mut foods: Vec<Food> = input.lines().map(|s| -> Food {
         let i = s.find('(').unwrap();
         Food { 
             ingredients: s[..(i-1)].split(' ').collect(), 
             allergens: s[(i+10)..(s.len()-1)].split(", ").collect() 
         }
-    }
-}
-
-pub fn solve(input: &str) -> Solution {
-    let foods: Vec<Food> = input.lines().map(|s| Food::parse(s)).collect();
+    }).collect();
 
     let mut unknown_allergens: HashSet<&str> = foods.iter().map(|f| f.allergens.iter()).flatten().cloned().collect();
     let mut known_allergens: BTreeMap<&str,&str> = BTreeMap::new();
-    let mut matched_ingredients: HashSet<&str> = HashSet::new();
     
     while unknown_allergens.len() > 0 {
+        let j = unknown_allergens.len();
         unknown_allergens = unknown_allergens.into_iter().filter(|u| {
-            // Take the intersection of the ingredients list where this allergen occurs
             let foods_with_this_allergen: Vec<&Food> = foods.iter().filter(|f| f.allergens.contains(u)).collect();
-            assert!(foods_with_this_allergen.len() > 0);
-            let candidate_ingredients:Vec<&str> = foods_with_this_allergen.iter()
+            let candidate_ingredients:HashSet<&str> = foods_with_this_allergen.iter()
                 .skip(1)
-                .fold(foods_with_this_allergen[0].ingredients.clone(),|i,f| i.intersection(&f.ingredients).cloned().collect())
-                .difference(&matched_ingredients)
-                .cloned()
-                .collect();
+                .fold(foods_with_this_allergen[0].ingredients.clone(),|i,f| i.intersection(&f.ingredients).cloned().collect());
             
             if candidate_ingredients.len() == 1 {
-                known_allergens.insert(u, candidate_ingredients[0]);
-                matched_ingredients.insert(candidate_ingredients[0]);
+                let ingredient = candidate_ingredients.into_iter().next().unwrap();
+                for f in foods.iter_mut() { f.ingredients.remove(ingredient); }
+                known_allergens.insert(u, ingredient);
                 false
             } else {
                 true
             }
         }).collect();
+        assert!(j != unknown_allergens.len());
     }
-    let p1 = foods.iter()
-        .map(|food| food.ingredients
-            .difference(&matched_ingredients)
-            .count())
-        .sum::<usize>();
-        
+
+    let p1 = foods.iter().map(|food| food.ingredients.len()).sum::<usize>();
     let p2 = known_allergens.values().join(",");
 
     Solution::new(p1,p2)
