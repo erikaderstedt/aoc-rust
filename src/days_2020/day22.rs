be8ef57857ae74227ebb1664c19638437de60f87
Erik Aderstedt <erik@aderstedtsoftware.com>
2020-12-22 09:31:45 +0100

    Use seahash for day 22

--- src/days_2020/day22.rs
+++ src/days_2020/day22.rs
@@ -1,13 +1,12 @@
 use crate::common::Solution;
 use std::collections::HashSet;
-use std::hash::{Hash,Hasher};
-use std::collections::hash_map::DefaultHasher;
+use seahash::hash;
 
-fn score(cards: &Vec<usize>) -> usize {
-    cards.iter().rev().enumerate().map(|(i, c)| (i+1) * (*c)).sum::<usize>()
+fn score(cards: &Vec<u8>) -> usize {
+    cards.iter().rev().enumerate().map(|(i, c)| (i+1) * (*c as usize)).sum::<usize>()
 }
 
-fn play_combat(player1: &Vec<usize>, player2: &Vec<usize>) -> usize {
+fn play_combat(player1: &Vec<u8>, player2: &Vec<u8>) -> usize {
     let mut player1 = player1.clone();
     let mut player2 = player2.clone();
     loop {
@@ -26,47 +25,47 @@ fn play_combat(player1: &Vec<usize>, player2: &Vec<usize>) -> usize {
     }
 }
 
-enum Player { Player1,Player2 }
+enum Result { Player1, Player2 }
 
-fn play_recursive_combat(mut player1: Vec<usize>, mut player2: Vec<usize>) -> (usize,Player) {
+fn play_recursive_combat(player1: &mut Vec<u8>, player2: &mut Vec<u8>) -> Result {
     let mut previous: HashSet<u64> = HashSet::new();
     loop {
-        let mut hasher = DefaultHasher::new();
-        player1.hash(&mut hasher);
-        player2.hash(&mut hasher);
-        let h = hasher.finish();
-        if !previous.insert(h) { return (0, Player::Player1); }
-        let c1 = player1.remove(0);
-        let c2 = player2.remove(0);
-        let winner = if player1.len() >= c1 && player2.len() >= c2 {
-            play_recursive_combat(player1[..c1].to_vec(), player2[..c2].to_vec()).1
+        let h = hash(&player1[..]) * hash(&player2[..]);
+        if !previous.insert(h) { return Result::Player1; }
+        let c1 = player1.remove(0) as usize;
+        let c2 = player2.remove(0) as usize;
+        let result = if player1.len() >= c1 && player2.len() >= c2 {
+            let mut p1_clone = player1.iter().take(c1).cloned().collect();
+            let mut p2_clone = player2.iter().take(c2).cloned().collect();
+            play_recursive_combat(&mut p1_clone, &mut p2_clone)
         } else if c1 > c2 {
-            Player::Player1
+            Result::Player1
         } else {
-            Player::Player2
+            Result::Player2
         };
-        match winner {
-            Player::Player1 => {
-                player1.push(c1);
-                player1.push(c2);
+        match result {
+            Result::Player1 => {
+                player1.push(c1 as u8);
+                player1.push(c2 as u8);
             },
-            Player::Player2 => {
-                player2.push(c2);
-                player2.push(c1);
+            Result::Player2 => {
+                player2.push(c2 as u8);
+                player2.push(c1 as u8);
             },
         }
 
-        if player1.is_empty() { return (score(&player2), Player::Player2); }
-        if player2.is_empty() { return (score(&player1), Player::Player1); }
+        if player1.is_empty() { return Result::Player2; }
+        if player2.is_empty() { return Result::Player1; }
     }
 }
 
 pub fn solve(input: &str) -> Solution {
-    let player1: Vec<usize> = input.lines().skip(1).take_while(|&c| c != "").map(|l| l.parse::<usize>().unwrap()).collect();
-    let player2 :Vec<usize> = input.lines().skip_while(|&c| c != "Player 2:").skip(1).map(|l| l.parse::<usize>().unwrap()).collect();
+    let mut player1: Vec<u8> = input.lines().skip(1).take_while(|&c| c != "").map(|l| l.parse::<u8>().unwrap()).collect();
+    let mut player2 :Vec<u8> = input.lines().skip_while(|&c| c != "Player 2:").skip(1).map(|l| l.parse::<u8>().unwrap()).collect();
 
     let p1 = play_combat(&player1, &player2);
-    let p2 = play_recursive_combat(player1, player2).0;
+    let winner_p2 = play_recursive_combat(&mut player1, &mut player2);
+    let p2 = score(match winner_p2 { Result::Player1 => &player1, Result::Player2 => &player2 });
     
     Solution::new(p1,p2)
 }
