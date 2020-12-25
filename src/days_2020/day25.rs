87e52c3e9ae8ff7a2b56761af44bc4f04735042f
Erik Aderstedt <erik@aderstedtsoftware.com>
2020-12-25 06:34:32 +0100

    Day 25

--- src/days_2020/day25.rs
+++ src/days_2020/day25.rs
@@ -1,12 +1,31 @@
 use crate::common::Solution;
-use itertools::Itertools;
-use regex::Regex;
-use std::str::FromStr;
-use crate::common::parsed_from_each_line;
-use std::collections::HashMap;
 
-pub fn solve(_lines: &str) -> Solution {
+const VALUE: usize = 20_201_227;
 
+fn find_loop_size(key: usize, subject_number: usize) -> usize {
+    let mut i = 1;
+    let mut n = 0;
+    loop {
+        i = i*subject_number % VALUE;
+        n += 1;
+        if i == key { break n; }
+    }
+}
+
+fn transform_key(key: usize, loop_size: usize) -> usize {
+    let mut i = key;
+    for _n in 0..(loop_size-1) {
+        i = i * key % VALUE;        
+    }
+    i 
+}
+
+const CARD_PUBLIC_KEY: usize = 12092626;
+const DOOR_PUBLIC_KEY: usize = 4707356;
+pub fn solve(_input: &str) -> Solution {
+
+    let card_loop_size = find_loop_size(CARD_PUBLIC_KEY, 7);
+    let p1 = transform_key(DOOR_PUBLIC_KEY, card_loop_size);
     
-    Solution { part_1: "".to_string(), part_2: "".to_string() }
+    Solution { part_1: p1.to_string(), part_2: "".to_string() }
 }
