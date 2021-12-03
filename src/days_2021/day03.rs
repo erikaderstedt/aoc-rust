2cffe49917adbe787c0924019370c10f101a9d77
Erik Aderstedt <erik@aderstedtsoftware.com>
2021-12-03 09:59:14 +0100

    Day 2021-3 refactored

--- src/days_2021/day03.rs
+++ src/days_2021/day03.rs
@@ -2,14 +2,9 @@
 
 use crate::common::Solution;
 
+// Returns 1 if there are an equal number of 0's and 1's
 fn most_common_digit_in_position(numbers: &Vec<usize>, bit_position: usize) -> usize {
     if numbers.iter().fold(0, |t, n| t + ((n >> bit_position) & 1)) * 2 >= numbers.len() { 1 } else { 0 }
-}
-
-fn generic_rate_calculator(numbers: &Vec<usize>, num_digits: usize, a: usize) -> usize {
-    (0..num_digits).fold(0, |value, bit_position| 
-        value + if most_common_digit_in_position(&numbers, bit_position) == a { 1 << bit_position } else { 0 }
-    )
 }
 
 fn generic_rating_calculator(numbers: &Vec<usize>, num_digits: usize, a: usize) -> usize {
@@ -25,8 +20,9 @@ pub fn solve(input: &str) -> Solution {
     let numbers = input.lines().map(|line| usize::from_str_radix(line, 2).expect("Bad number")).collect();
     let num_digits = input.lines().next().unwrap().len();
 
-    let gamma = generic_rate_calculator(&numbers, num_digits, 1);
-    let epsilon = generic_rate_calculator(&numbers, num_digits, 0);
+    let gamma = (0..num_digits).fold(0, |value, bit_position| 
+        value + (most_common_digit_in_position(&numbers, bit_position) << bit_position));
+    let epsilon = (!gamma) & ((1 << num_digits) - 1);
     let m1 = gamma * epsilon;
 
     let oxygen_generator_rating = generic_rating_calculator(&numbers, num_digits, 1);
