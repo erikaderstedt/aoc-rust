e0d670de37a00af25b3ed6f77c288d47794d1213
Erik Aderstedt <erik@aderstedtsoftware.com>
2021-12-04 12:38:02 +0100

    Day 2021-4 refactored

--- src/days_2021/day04.rs
+++ src/days_2021/day04.rs
@@ -19,7 +19,6 @@ fn sum_of_unmarked_numbers(board: &Board, drawn_numbers: &u128) -> usize {
 }
 
 pub fn solve(input: &str) -> Solution {
-
     let bingo_numbers: Vec<usize> = input.lines().next().unwrap().split(',').map(|p| p.parse::<usize>().unwrap()).collect();
     let boards: Vec<Board> = input.split("\n\n").skip(1).map(|chunk| {
         let n: Vec<u128> = chunk.split_whitespace().filter_map(|p| p.parse::<u128>().ok()).collect();
@@ -35,27 +34,26 @@ pub fn solve(input: &str) -> Solution {
         (1<<n[4]) + (1<<n[9]) + (1<<n[14]) + (1<<n[19]) + (1<<n[24])]
     }).collect();
 
-    let m1 = bingo_numbers.iter().scan(0u128, |drawn_numbers, &num| {
-        *drawn_numbers |= 1 << num;
-        Some(match boards.iter().find(|board| board.iter().any(|w| w & *drawn_numbers == *w)) {
-            Some(board) => Some(sum_of_unmarked_numbers(board, drawn_numbers) * num),
-            _ => None,
-        })
-    }).skip_while(|w| w.is_none() ).next().unwrap().unwrap();
-      
-    let m2 = bingo_numbers.iter().scan(0u128, |drawn_numbers, &num| {
-        *drawn_numbers |= 1 << num;
-        Some(if boards.iter().all(|board| board.iter().any(|w| w & *drawn_numbers == *w)) {
-            // All boards win. Remove this number and check which board does not win.
-            *drawn_numbers -= 1 << num;
-            boards.iter().find_map(|board| 
-                if !board.iter().any(|w| w & *drawn_numbers == *w) { 
-                    Some(sum_of_unmarked_numbers(board, &(*drawn_numbers | (1 << num))) * num)
-                } else { 
-                    None 
-                })
-            } else { None })
-    }).skip_while(|w| w.is_none() ).next().unwrap().unwrap();
+    let mut drawn_numbers = 0u128;
+    let mut m1: Option<usize> = None;
+    let mut m2: Option<usize> = None;
+    for num in bingo_numbers.iter() {
+        drawn_numbers |= 1 << num;
+        if m1 == None {
+            m1 = match boards.iter()
+                .find(|board| board.iter().any(|w| w & drawn_numbers == *w)) {
+                Some(board) => Some(sum_of_unmarked_numbers(board, &drawn_numbers) * num),
+                _ => None,
+            };
+        } else if boards.iter().all(|board| board.iter().any(|w| w & drawn_numbers == *w)) {
+            m2 = match boards.iter()
+                .find(|board| !board.iter().any(|w| w & (drawn_numbers - (1 << num)) == *w)) {
+                Some(board) => Some(sum_of_unmarked_numbers(board, &drawn_numbers) * num),
+                _ => None,
+            };
+            break
+        }
+    }
 
-    Solution::new(m1, m2)
+    Solution::new(m1.unwrap(), m2.unwrap())
 }
