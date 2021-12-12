18e1556ebe053544d6265d876fe30c540d38ba6e
Erik Aderstedt <erik@aderstedtsoftware.com>
2021-12-12 07:02:31 +0100

    Day 2021 12

--- src/days_2021/day11.rs
+++ src/days_2021/day11.rs
@@ -4,47 +4,41 @@ use crate::common::Solution;
 const SIZE: usize = 10;
 const ALL: usize = SIZE*SIZE;
 
+fn step(energy_levels: &mut [u32;ALL]) -> usize {
+    let mut not_flashed = [true;ALL];
+    while match energy_levels.iter().enumerate().position(|(i, o)| not_flashed[i] && *o > 9) {
+        Some(position) => {
+            let not_left_edge = position%SIZE > 0;
+            let not_right_edge = position%SIZE < SIZE - 1;
+            let not_top_edge = position >= SIZE;
+            let not_bottom_edge = position < ALL - SIZE;
+            if not_left_edge { energy_levels[position - 1] += 1; }
+            if not_left_edge && not_top_edge { energy_levels[position - SIZE - 1] += 1; }
+            if not_left_edge && not_bottom_edge { energy_levels[position + SIZE - 1] += 1; }
+            if not_top_edge { energy_levels[position - SIZE] += 1; }
+            if not_bottom_edge { energy_levels[position + SIZE] += 1; }
+            if not_right_edge { energy_levels[position + 1] += 1; }
+            if not_right_edge && not_top_edge { energy_levels[position - SIZE + 1] += 1; }
+            if not_right_edge && not_bottom_edge { energy_levels[position + SIZE + 1] += 1; }
+            not_flashed[position] = false;
+            true
+        },
+        None => false,
+    } {}
+    energy_levels.iter_mut().for_each(|o| if *o > 9 { *o = 1 } else { *o += 1 });
+    ALL - not_flashed.iter().filter(|f| **f).count()
+}
+
 pub fn solve(input: &str) -> Solution {
-    let mut octopuses = [0u32;SIZE*SIZE];
+    let mut energy_levels = [0u32;ALL];
     for (y, line) in input.lines().enumerate() {
         for (x, &c) in line.as_bytes().iter().enumerate() {
-            octopuses[y*SIZE+x] = (c as u32) - 47;
+            energy_levels[y*SIZE+x] = (c as u32) - 47; // Start at 1
         }
     }
 
-    let mut m1 = 0;
-    let m2: usize;
-    let mut step = 1; 
-    loop {
-        let mut flashed = [false;SIZE*SIZE];
-        while match octopuses.iter().enumerate().position(|(i,o)| !flashed[i] && *o > 9) {
-            Some(position) => {
-                let not_left_edge = position%SIZE > 0;
-                let not_right_edge = position%SIZE < SIZE - 1;
-                let not_top_edge = position >= SIZE;
-                let not_bottom_edge = position < ALL - SIZE;
-                if not_left_edge { octopuses[position - 1] += 1; }
-                if not_left_edge && not_top_edge { octopuses[position - SIZE - 1] += 1; }
-                if not_left_edge && not_bottom_edge { octopuses[position + SIZE - 1] += 1; }
-                if not_top_edge { octopuses[position - SIZE] += 1; }
-                if not_bottom_edge { octopuses[position + SIZE] += 1; }
-                if not_right_edge { octopuses[position + 1] += 1; }
-                if not_right_edge && not_top_edge { octopuses[position - SIZE + 1] += 1; }
-                if not_right_edge && not_bottom_edge { octopuses[position + SIZE + 1] += 1; }
-                flashed[position] = true;
-                if step <= 100 { m1 += 1; }
-                true
-            },
-            None => false,
-        } {}
-        if flashed.iter().all(|f| *f) {
-            m2 = step;
-            break;
-        }
-        // Set all flashed to 0
-        octopuses.iter_mut().for_each(|o| if *o > 9 { *o = 1 } else { *o = *o + 1 });
-        step += 1;
-    }
+    let m1 = (0..100).fold(0, |s, _| s + step(&mut energy_levels));
+    let m2 = (101..).find(|_| step(&mut energy_levels) == ALL).unwrap();
     
     Solution::new(m1, m2)
 }
---
+++ src/days_2021/day12.rs
@@ -0,0 +1,95 @@
+// https://adventofcode.com/2021/day/12
+use crate::common::Solution;
+use itertools::Itertools;
+
+#[derive(Copy,Clone,PartialEq,Debug)]
+enum Cave<'a> {
+    Start,
+    End,
+    Large(&'a str),
+    Small(&'a str),
+}
+
+impl<'a> Cave<'a> {
+    fn from(s: &str) -> Cave {
+        match s {
+            "start" => Cave::Start,
+            "end" => Cave::End,
+            _ => { if s.as_bytes()[0] >= ('a' as u8) { 
+                Cave::Small(s)
+            } else {
+                Cave::Large(s)
+            }}
+        }
+    }
+}
+
+#[derive(Debug)]
+struct CaveSystem<'a> {
+    caves: Vec<(Cave<'a>,Vec<usize>)>,
+}
+
+impl<'a> CaveSystem<'a> {
+    fn from(s: &str) -> CaveSystem {
+        // Build vec of caves
+        let cave_names: Vec<&str> = s.lines()
+            .flat_map(|line| line.split('-'))
+            .unique()
+            .collect();
+        CaveSystem { 
+            caves: cave_names.iter().map(|&name| {
+                let destination_indices = s.lines()
+                    .filter_map(|line| {
+                        let (start, end) = line.split_once('-').unwrap();
+                        if name == start {
+                            Some(end)
+                        } else if name == end && name != "end" && start != "start" {
+                            Some(start)
+                        } else {
+                            None
+                        }})
+                    .map(|n| cave_names.iter().position(|&c| c == n).unwrap())
+                    .collect();
+                (Cave::from(name), destination_indices)
+            }).collect()
+        }
+    }
+
+    fn explore(&self, allows_revisits_of_small_caves: bool) -> usize {
+        let mut available_actions: Vec<(usize, bool,usize)> = self.caves[0].1.iter().map(
+            |destination_index| {
+                (1 << 0, allows_revisits_of_small_caves, *destination_index) 
+            }
+        ).collect();
+        
+        let mut num_paths = 0;
+        while let Some((visited, allows_revisits, next_index)) = available_actions.pop() {
+            match self.caves[next_index].0 {
+                Cave::Start => { panic!("We should not have a path leading back to start.") },
+                Cave::End => { num_paths += 1; },
+                Cave::Large(_) => {
+                    available_actions.extend(self.caves[next_index].1.iter().map(|destination_index|{
+                        (visited | (1 << next_index), allows_revisits, *destination_index) 
+                    })); },
+                Cave::Small(_) if allows_revisits || (visited & (1 << next_index)) == 0 => {
+                    available_actions.extend(self.caves[next_index].1.iter().map(|destination_index|{
+                        (visited | (1 << next_index), 
+                        if (visited & (1 << next_index)) == 0 { allows_revisits } else { false}, 
+                        *destination_index) 
+                    }));
+                },
+                _ => {},
+            }
+        }
+        num_paths
+    }
+}
+
+pub fn solve(input: &str) -> Solution {
+    let cave_system = CaveSystem::from(input);
+
+    let m1 = cave_system.explore(false);
+    let m2 = cave_system.explore(true);
+
+    Solution::new(m1,m2)
+}
