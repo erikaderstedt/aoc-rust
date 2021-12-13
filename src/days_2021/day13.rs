ba9d6b820ea4febd219cef50ab28bea839e37bb0
Erik Aderstedt <erik@aderstedtsoftware.com>
2021-12-13 20:36:35 +0100

    Day 2021 13 refactored again

--- src/days_2021/day13.rs
+++ src/days_2021/day13.rs
@@ -7,10 +7,86 @@ enum Fold {
     AlongY(usize),
 }
 
-const WIDTH: usize = 40;
-const HEIGHT: usize = 7;
+fn final_folded_index(i: usize, size: usize) -> usize {
+    let n = (i + size + 1)/(size*2+2);
+    if i < (size+1)*2*n {
+        (size+1)*2*n-2-i
+    } else {
+        i - (size+1)*2*n
+    }
+}
 
-fn ocr(grid: &[[bool;40];HEIGHT], x: usize) -> char {
+pub fn solve(input: &str) -> Solution {
+    // Size of grid is first x fold * 2 + 1, and first y fold * 2 + 1
+    // folds are always in the middle.
+    let folds: Vec<Fold> = input.lines().skip_while(|line| *line != "").filter_map(|line| {
+        match line.split_once('=') {
+            Some((_,b)) => {
+                let l = b.parse::<usize>().unwrap();
+                match line.as_bytes()[11] {
+                    b'y' => Some(Fold::AlongY(l)),
+                    b'x' => Some(Fold::AlongX(l)),
+                    _ => panic!("Bad format"),
+                }
+            },
+            None => None,
+        }
+    }).collect();
+
+    let num_original_points = input.lines().count() - 1 - folds.len();
+    let grid_points: Vec<(usize,usize)> = input.lines().take(num_original_points).map(|line| {
+        let (x,y) = line.split_once(',').unwrap();
+        (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap())
+    }).collect();
+
+    // For pt 1, use HashSet of coordinates because the grid will be too large and slow.
+    let m1 = {
+        let points: HashSet<(usize,usize)> = match folds[0] {
+        Fold::AlongX(x0) => grid_points.iter().map(|(x,y)| {
+                                    (if *x < x0 { *x } else { 2*x0 - x }, *y)
+                            }).collect(),
+        Fold::AlongY(y0) => grid_points.iter().map(|(x,y)| {
+                                    (*x, if *y < y0 { *y } else { 2*y0 - y })
+                            }).collect()
+                        };
+        points.len()
+    };
+
+    let m2 = {
+        // For pt 2, use a grid. Grid will be the size of the last folds in each direction
+        let final_x_width = folds.iter()
+            .filter_map(|f| match f {
+                Fold::AlongX(x) => Some(*x),
+                _ => None
+            }).last().unwrap();
+        let final_y_width = folds.iter()
+            .filter_map(|f| match f {
+                Fold::AlongY(y) => Some(*y),
+                _ => None
+            }).last().unwrap();
+
+        let mut grid = vec![vec![false;final_x_width];final_y_width];
+        for (x, y) in grid_points.into_iter() {
+            grid[final_folded_index(y, final_y_width)][final_folded_index(x, final_x_width)] = true;
+        }
+        if final_x_width == 40 && final_y_width == 6 {
+            (0..8).map(|i| ocr(&grid, i*5)).collect::<String>()
+        } else {
+            println!("");
+            for line in grid.into_iter() {
+                for c in line {
+                    print!("{}", if c { '█' } else { ' ' });
+                }
+                println!("");
+            }
+            "?????".to_string()
+        }
+    };
+
+    Solution::new(m1,m2)
+}
+
+fn ocr(grid: &Vec<Vec<bool>>, x: usize) -> char {
     if grid[1][x+3] {
         if grid[3][x+1] {
             if grid[0][x+3] {
@@ -44,91 +120,3 @@ fn ocr(grid: &[[bool;40];HEIGHT], x: usize) -> char {
 	else if grid[5][x+2] { 'I' }
     else if grid[0][x+3] { 'F' } else { 'T' }
 }
-
-const ORIGINAL_X_SIZE: usize = 1306;
-const ORIGINAL_Y_SIZE: usize = 894;
-fn create_coordinate_maps<'a,I>(folds: I) -> (Vec<usize>, Vec<usize>) 
-where I: Iterator<Item=&'a Fold> {
-    let mut x_map = Vec::with_capacity(ORIGINAL_X_SIZE);
-    let mut y_map = Vec::with_capacity(ORIGINAL_Y_SIZE);
-    for i in 0..ORIGINAL_X_SIZE { 
-        x_map.push(i);
-        y_map.push(i);
-    }
-    for i in 0..ORIGINAL_Y_SIZE { 
-        y_map.push(i);
-    }
-    let mut x_size = ORIGINAL_X_SIZE;
-    let mut y_size = ORIGINAL_Y_SIZE;
-    for fold in folds {
-        match fold {
-            Fold::AlongY(y) => {
-                y_map[*y] = 0;
-                for i in (y+1)..y_size {
-                    y_map[i] = 2*y - i;
-                }
-                for i in y_size..ORIGINAL_Y_SIZE {
-                    y_map[i] = y_map[y_map[i]];
-                }
-                y_size = *y;
-            },
-            Fold::AlongX(x) => {
-                x_map[*x] = 0;
-                for i in (x+1)..x_size {
-                    x_map[i] = 2*x - i;
-                }
-                for i in x_size..ORIGINAL_X_SIZE {
-                    x_map[i] = x_map[x_map[i]];
-                }
-                x_size = *x;
-            },
-        }
-    }
-    (x_map, y_map)
-}
-pub fn solve(input: &str) -> Solution {
-
-    let folds: Vec<Fold> = input.lines().filter_map(|line| {
-        match line.split_once('=') {
-            Some((_,b)) => {
-                let l = b.parse::<usize>().unwrap();
-                match line.as_bytes()[11] {
-                    b'y' => Some(Fold::AlongY(l)),
-                    b'x' => Some(Fold::AlongX(l)),
-                    _ => panic!("Bad format"),
-                }
-            },
-            None => None,
-        }
-    }).collect();
-
-    let num_original_points = input.lines().count() - 1 - folds.len();
-    let grid_points: Vec<(usize,usize)> = input.lines().take(num_original_points).map(|line| {
-        let (x,y) = line.split_once(',').unwrap();
-        (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap())
-    }).collect();
-
-    // For pt 1, use HashSet of coordinates because the grid will be too large and slow.
-    let m1 = {
-        let (x_map, y_map) = create_coordinate_maps(folds.iter().take(1));
-        let points: HashSet<(usize,usize)> = grid_points.iter().map(|(x,y)| {
-            (x_map[*x], y_map[*y])
-        }).collect();
-        points.len()
-    };
-
-    let m2 = {
-        // For pt 2, use a grid. Grid will be the size of the last folds in each direction
-        // For now, hard code these
-        let (x_map, y_map) = create_coordinate_maps(folds.iter());
-        let mut grid = [[false;WIDTH];HEIGHT];
-        for (x0, y0) in grid_points.into_iter() {
-            grid[y_map[y0]][x_map[x0]] = true;
-        }
-        (0..8).map(|i| ocr(&grid, i*5)).collect::<String>()
-    };
-
-    Solution::new(m1,m2)
-}
-
-
