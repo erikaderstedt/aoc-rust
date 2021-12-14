def268fd3420008df81db015daf0cafede2fd8e4
Erik Aderstedt <erik.aderstedt@hasselblad.com>
2021-12-14 09:59:52 +0100

    Day 2021-14 refactored

--- src/days_2021/day14.rs
+++ src/days_2021/day14.rs
@@ -1,56 +1,71 @@
-// https://adventofcode.com/2021/day/12
+// https://adventofcode.com/2021/day/14
 use crate::common::Solution;
-use std::collections::HashMap;
-type Pair = (u8,u8);
-type Polymer = HashMap<Pair,usize>;
 
-fn most_vs_least_common(polymer: &Polymer, initial_byte: u8) -> usize {
-	let mut counts: HashMap<u8, usize> = HashMap::new();
-	for (pair, count) in polymer.iter() {
-		*counts.entry(pair.1).or_insert(0) += count;
+const NUM_ELEMENTS: usize = 10;
+const NUM_PAIRS: usize = NUM_ELEMENTS * NUM_ELEMENTS;
+fn element_index(element: u8) -> usize {
+	match element {
+		b'B' => 0,
+		b'C' => 1,
+		b'F' => 2,
+		b'H' => 3,
+		b'K' => 4,
+		b'N' => 5,
+		b'O' => 6,
+		b'P' => 7,
+		b'S' => 8,
+		b'V' => 9,
+		_ => panic!("Unrecognized element"),
 	}
-	*counts.entry(initial_byte).or_insert(0) += 1;
-	let most_common: usize = counts.iter().map(|(_, c)| *c).max().unwrap();
-	let least_common: usize = counts.iter().map(|(_, c)| *c).min().unwrap();
+}
+
+type Polymer = [usize;NUM_PAIRS];
+type ReactionTable = [(usize,usize);NUM_PAIRS];
+
+fn most_vs_least_common(polymer: &Polymer, initial_element: usize) -> usize {
+	let mut counts = [0usize; NUM_ELEMENTS];
+	counts[initial_element] = 1;
+	for (pair, count) in polymer.iter().enumerate() {
+		counts[pair % NUM_ELEMENTS] += count;
+	}
+	let most_common: usize = *counts.iter().max().unwrap();
+	let least_common: usize = *counts.iter().filter(|&&c| c > 0).min().unwrap();
 	most_common - least_common
 }
 
-fn update(polymer: Polymer, reaction_pairs: &HashMap<Pair,(Pair,Pair)>) -> Polymer {
-	let mut new_polymer: Polymer = HashMap::new();
-	for (pair, count) in polymer.into_iter() {
-		let (p1,p2) = reaction_pairs[&pair];
-		*new_polymer.entry(p1).or_insert(0) += count;
-		*new_polymer.entry(p2).or_insert(0) += count;
+fn update(polymer: Polymer, reaction_pairs: &ReactionTable) -> Polymer {
+	let mut new_polymer: Polymer = [0usize;NUM_PAIRS];
+	for (pair, count) in polymer.iter().enumerate() {
+		if *count > 0 {
+			let r = reaction_pairs[pair];
+			new_polymer[r.0] += count;
+			new_polymer[r.1] += count;
+		}
 	}
 	new_polymer
 }
 
 pub fn solve(input: &str) -> Solution {
-	let reaction_pairs: HashMap<Pair,(Pair,Pair)> = input
-		.lines()
-		.skip(2)
-		.map(|line| {
-			let c = line.as_bytes();
-			((c[0], c[1]), ((c[0], c[6]),(c[6], c[1])))
-		}).collect();
+	let mut reaction_table: ReactionTable = [(0usize,0usize);NUM_PAIRS];
+	for line in input.lines().skip(2) {
+		let p1 = element_index(line.as_bytes()[0]);
+		let p2 = element_index(line.as_bytes()[1]);
+		let r = element_index(line.as_bytes()[6]);
+		reaction_table[p1 * NUM_ELEMENTS + p2] = 
+			(p1 * NUM_ELEMENTS + r, r * NUM_ELEMENTS + p2);
+	}
 
-	let mut polymer: Polymer = HashMap::new();
-	for pair in input
-		.lines()
-		.next()
-		.unwrap()
-		.as_bytes()
-		.windows(2) {
-			*polymer.entry((pair[0], pair[1])).or_insert(0) += 1;
-		}
-	let initial_byte: u8 = input.as_bytes()[0];
+	let mut polymer = [0usize;NUM_PAIRS];
+	for pair in input.lines().next().unwrap().as_bytes().windows(2) {
+		polymer[element_index(pair[0]) * NUM_ELEMENTS + element_index(pair[1])] += 1;
+	}
 
-	for _step in 0..10 { polymer = update(polymer, &reaction_pairs); }
+	let initial_byte: usize = element_index(input.as_bytes()[0]);
+
+	for _step in 0..10 { polymer = update(polymer, &reaction_table); }
 	let m1 = most_vs_least_common(&polymer, initial_byte);
-	for _step in 10..40 { polymer = update(polymer, &reaction_pairs); }
+	for _step in 10..40 { polymer = update(polymer, &reaction_table); }
 	let m2 = most_vs_least_common(&polymer, initial_byte);
 
     Solution::new(m1,m2)
 }
-
-
