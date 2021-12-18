f924e260d9d832b8c4b03678bd38fe0cdc5569a0
Erik Aderstedt <erik@aderstedtsoftware.com>
2021-12-18 20:08:18 +0100

    Day 2021 18 refactored

--- src/days_2021/day18.rs
+++ src/days_2021/day18.rs
@@ -1,6 +1,5 @@
 // https://adventofcode.com/2021/day/18
 use crate::common::Solution;
-use itertools::Itertools;
 use std::iter::Peekable;
 use std::fmt;
 
@@ -20,20 +19,17 @@ struct Snailfish {
 
 impl Snailfish {
 
-    fn from(c: &[u8]) -> Snailfish {
+    fn from(bytes: &[u8]) -> Snailfish {
         let mut level = 0;
-        let len = c.len();
-        let mut nums = Vec::new();
-        for i in 0..len {
-            match c[i] {
-                b'[' => { level += 1; },
-                b']' => { level -= 1; },
-                b',' => { },
-                b'0'..=b'9' => { nums.push(Number { value: (c[i] - b'0') as Value, level: level})},
+        Snailfish { n: bytes.iter().filter_map(|c|
+            match c {
+                b'[' => { level += 1; None },
+                b']' => { level -= 1; None },
+                b',' => { None },
+                b'0'..=b'9' => { Some(
+                    Number { value: (c - b'0') as Value, level: level } ) },
                 _ => { panic!("??"); }
-            }
-        }
-        Snailfish { n: nums }
+            }).collect() }
     }
 
     fn magnitude(&self) -> Value {
@@ -51,52 +47,74 @@ impl Snailfish {
 
     fn inner_magnitude<'a, I>(numbers: &mut Peekable<I>, base_level: Level) -> Value 
         where I: Iterator<Item=&'a Number> {
-        Snailfish::next(numbers, base_level) * 3 + Snailfish::next(numbers, base_level) * 2
+        Snailfish::next(numbers, base_level) * 3 + 
+        Snailfish::next(numbers, base_level) * 2
     }
 
+    fn explode<const PRE: bool>(n: &mut Vec<Number>) {
+        let level = if PRE { 3 } else { 4 };
+        while let Some(i) = n.iter().position(|x| x.level > level) {
+            if i < n.len()-2 {
+                n[i+2].value += n[i+1].value;
+            } else if PRE {
+                break
+            }
+
+            if i > 0 {
+                n[i-1].value += n[i].value;
+            }
+
+            n.remove(i+1);
+            n[i] = Number { level: n[i].level - 1, value: 0 };
+        }
+    }
+
+    fn split(n: &mut Vec<Number>) -> bool {
+        if let Some(i) = n.iter().position(|x| x.value >= 10) {
+            let level = n[i].level + 1;
+            let value = n[i].value;
+            if level > 4 {
+                // This pair will explode. Do it directly without the insert and
+                // remove operations.
+                if i > 0 { n[i-1].value += value/2; }
+                if i < n.len()-1 { n[i+1].value += value - value/2; }
+                n[i].value = 0;
+            } else {
+                n[i] = Number { level, value: value/2 };
+                n.insert(i+1, Number { level, value: value - value/2 });
+            }
+            true
+        } else {
+            false
+        }
+    }
+    
     fn add(&self, other: &Snailfish) -> Snailfish {
         let mut combined: Vec<Number> = self.n.iter()
             .chain(other.n.iter())
-            .cloned()        
+            .map(|n| Number { value: n.value, level: n.level + 1 })
             .collect();
-        for f in combined.iter_mut() { f.level += 1; }
 
         loop {
-            if let Some(i) = combined.iter().position(|x| x.level > 4) {
-                if i > 0 {
-                    combined[i-1].value += combined[i].value;
-                }
-                if i + 1 < combined.len()-1 {
-                    combined[i+2].value += combined[i+1].value;
-                }
-                combined.remove(i+1);
-                combined[i].level -= 1;
-                combined[i].value = 0;
-                continue;
-            }
-
-            if let Some(i) = combined.iter().position(|x| x.value >= 10) {
-                let value = combined[i].value;
-                combined[i].value = value/2;
-                combined[i].level += 1;
-                combined.insert(i+1, Number {
-                    level: combined[i].level,
-                    value: value - combined[i].value });
-                continue;
-            }
+            Snailfish::explode::<false>(&mut combined);
+            if Snailfish::split(&mut combined) { continue }
             break;
         }
         Snailfish { n: combined }
     }
 }
 
+
 pub fn solve(input: &str) -> Solution {
     let fishies: Vec<Snailfish> = input.lines().map(|s| Snailfish::from(s.as_bytes())).collect();
     let m1 = fishies.iter().fold(fishies[0].clone(), |f1, f2| f1.add(f2)).magnitude();
-    let m2 = fishies.into_iter().permutations(2).map(|f| -> Value {
-        f[0].add(&f[1]).magnitude()
+
+    let right_side = fishies.clone();
+    let m2 = fishies.into_iter().map(|mut fish| {
+        Snailfish::explode::<true>(&mut fish.n);
+        right_side.iter().map(|o| fish.add(o).magnitude()).max().unwrap()
     }).max().unwrap();
-    
+
     Solution::new(m1,m2)
 }
 
@@ -112,7 +130,7 @@ impl fmt::Display for Snailfish {
 impl Snailfish {
     
     fn display<'a, I>(formatter: &mut fmt::Formatter, numbers: &mut Peekable<I>, base_level: Level) -> Result<(),std::fmt::Error> 
-    where I: Iterator<Item=&'a Number>
+        where I: Iterator<Item=&'a Number>
     {
         if numbers.peek().unwrap().level > base_level {
             write!(formatter, "[")?;
