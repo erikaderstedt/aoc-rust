1bce6ba78ae11b2440b390e9824c9a9074491d2f
Erik Aderstedt <erik@aderstedtsoftware.com>
2021-12-18 12:57:51 +0100

    Added refactored fmt::Display implementation

--- src/days_2021/day18.rs
+++ src/days_2021/day18.rs
@@ -2,11 +2,15 @@
 use crate::common::Solution;
 use itertools::Itertools;
 use std::iter::Peekable;
+use std::fmt;
+
+type Value = u16;
+type Level = u16;
 
 #[derive(Clone)]
 struct Number {
-    value: u8,
-    level: usize,
+    value: Value,
+    level: Level,
 }
 
 #[derive(Clone)]
@@ -17,7 +21,7 @@ struct Snailfish {
 impl Snailfish {
 
     fn from(c: &[u8]) -> Snailfish {
-        let mut level: usize = 0;
+        let mut level = 0;
         let len = c.len();
         let mut nums = Vec::new();
         for i in 0..len {
@@ -25,27 +29,27 @@ impl Snailfish {
                 b'[' => { level += 1; },
                 b']' => { level -= 1; },
                 b',' => { },
-                b'0'..=b'9' => { nums.push(Number { value: c[i] - b'0', level: level})},
+                b'0'..=b'9' => { nums.push(Number { value: (c[i] - b'0') as Value, level: level})},
                 _ => { panic!("??"); }
             }
         }
         Snailfish { n: nums }
     }
 
-    fn magnitude(&self) -> usize {
+    fn magnitude(&self) -> Value {
         Snailfish::inner_magnitude(&mut self.n.iter().peekable(), 1)
     }
 
-    fn next<'a, I>(numbers: &mut Peekable<I>, base_level: usize) -> usize 
+    fn next<'a, I>(numbers: &mut Peekable<I>, base_level: Level) -> Value
         where I: Iterator<Item=&'a Number> {
         if numbers.peek().unwrap().level > base_level {
             Snailfish::inner_magnitude(numbers, base_level + 1)
         } else { 
-            numbers.next().unwrap().value as usize
+            numbers.next().unwrap().value as Value
         }
     }
 
-    fn inner_magnitude<'a, I>(numbers: &mut Peekable<I>, base_level: usize) -> usize 
+    fn inner_magnitude<'a, I>(numbers: &mut Peekable<I>, base_level: Level) -> Value 
         where I: Iterator<Item=&'a Number> {
         Snailfish::next(numbers, base_level) * 3 + Snailfish::next(numbers, base_level) * 2
     }
@@ -88,11 +92,47 @@ impl Snailfish {
 
 pub fn solve(input: &str) -> Solution {
     let fishies: Vec<Snailfish> = input.lines().map(|s| Snailfish::from(s.as_bytes())).collect();
-    
     let m1 = fishies.iter().fold(fishies[0].clone(), |f1, f2| f1.add(f2)).magnitude();
-    let m2 = fishies.into_iter().permutations(2).map(|f| -> usize {
+    let m2 = fishies.into_iter().permutations(2).map(|f| -> Value {
         f[0].add(&f[1]).magnitude()
     }).max().unwrap();
     
     Solution::new(m1,m2)
+}
+
+impl fmt::Display for Snailfish {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[")?;
+        Snailfish::display(f, &mut self.n.iter().peekable(), 1).unwrap();
+        write!(f, "]")?;
+        Ok(())
+    }
+}
+
+impl Snailfish {
+    
+    fn display<'a, I>(formatter: &mut fmt::Formatter, numbers: &mut Peekable<I>, base_level: Level) -> Result<(),std::fmt::Error> 
+    where I: Iterator<Item=&'a Number>
+    {
+        if numbers.peek().unwrap().level > base_level {
+            write!(formatter, "[")?;
+            Snailfish::display(formatter, numbers, base_level + 1)?;
+            write!(formatter, "]")?;
+        } else { 
+            write!(formatter, "{}", numbers.next().unwrap().value)?;
+        }
+
+        write!(formatter, ",")?;
+    
+        if numbers.peek().unwrap().level > base_level {
+            write!(formatter, "[")?;
+            Snailfish::display(formatter, numbers, base_level + 1)?;
+            write!(formatter, "]")?;
+        } else { 
+            write!(formatter, "{}", numbers.next().unwrap().value)?;
+        }
+
+        Ok(())
+    }
+
 }
