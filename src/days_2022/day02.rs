4c3a9371e51469234d01f292a6a4e84546e44863
Erik Aderstedt <erik@aderstedt.se>
2022-12-02 11:42:54 +0100

    Constant definitions for day 2

--- src/days_2022/day02.rs
+++ src/days_2022/day02.rs
@@ -2,20 +2,28 @@
 
 use crate::common::Solution;
 
+const ROCK: usize = 1;
+const PAPER: usize = 2;
+const SCISSORS: usize = 3;
+
+const WIN: usize = 6;
+const TIE: usize = 3;
+const LOSS: usize = 0;
+
 pub fn solve(input: &str) -> Solution {
     let (p1,p2) = input
         .split("\n")
         .flat_map(|line| {
             match line {
-                "A X" => Some((1 + 3, 3 + 0)),
-                "A Y" => Some((2 + 6, 1 + 3)),
-                "A Z" => Some((3 + 0, 2 + 6)),
-                "B X" => Some((1 + 0, 1 + 0)),
-                "B Y" => Some((2 + 3, 2 + 3)),
-                "B Z" => Some((3 + 6, 3 + 6)),
-                "C X" => Some((1 + 6, 2 + 0)),
-                "C Y" => Some((2 + 0, 3 + 3)),
-                "C Z" => Some((3 + 3, 1 + 6)),
+                "A X" => Some((ROCK + TIE,      SCISSORS + LOSS)),
+                "A Y" => Some((PAPER + WIN,     ROCK + TIE)),
+                "A Z" => Some((SCISSORS + LOSS, PAPER + WIN)),
+                "B X" => Some((ROCK + LOSS,     ROCK + LOSS)),
+                "B Y" => Some((PAPER + TIE,     PAPER + TIE)),
+                "B Z" => Some((SCISSORS + WIN,  SCISSORS + WIN)),
+                "C X" => Some((ROCK + WIN,      PAPER + LOSS)),
+                "C Y" => Some((PAPER + LOSS,    SCISSORS + TIE)),
+                "C Z" => Some((SCISSORS + TIE,  ROCK + WIN)),
                 _ => None
             }})
         .fold((0,0), |sum, scores| (sum.0 + scores.0, sum.1 + scores.1));    
