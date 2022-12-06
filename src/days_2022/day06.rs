3b4facd16bdbb4d7a332ee56debcb106c71f1650
Erik Aderstedt <erik@aderstedt.se>
2022-12-06 10:43:40 +0100

    Day 6 a bit faster

--- src/days_2022/day06.rs
+++ src/days_2022/day06.rs
@@ -3,13 +3,14 @@
 use crate::common::Solution;
 
 fn check(input: &[u8], window_size: usize) -> usize {
-    (0..(input.len() - window_size))
-        .find(|i|
-            (0..(window_size - 1))
-                .find(|j| input[(i + j + 1)..(i+window_size)].contains(&input[i + j]))
-                .is_none()
-        )
-        .unwrap() + window_size
+    let mut i = 0;
+    loop {
+        match (0..(window_size - 1))
+                .find(|j| input[(i + j + 1)..(i + window_size)].contains(&input[i + j])) {
+            Some(k) => i = i + k + 1,
+            None => { return i + window_size; }
+        }
+    }
 }
 
 pub fn solve(input: &str) -> Solution {
