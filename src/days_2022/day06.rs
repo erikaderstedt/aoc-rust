de7dab61e7a36d4b96bb20fcd043cc0b2724b650
Erik Aderstedt <erik@aderstedt.se>
2022-12-06 07:26:35 +0100

    Day 6 refactored

--- src/days_2022/day06.rs
+++ src/days_2022/day06.rs
@@ -1,23 +1,20 @@
 // https://adventofcode.com/2022/day/6
 
-use itertools::Itertools;
 use crate::common::Solution;
 
-fn check(input: &str, window_size: usize) -> usize {
-    input.as_bytes()
-        .windows(window_size)
-        .find_position(|w|
+fn check(input: &[u8], window_size: usize) -> usize {
+    (0..(input.len() - window_size))
+        .find(|i|
             (0..(window_size - 1))
-                .find(|i| w[(i+1)..].contains(&w[*i]))
+                .find(|j| input[(i + j + 1)..(i+window_size)].contains(&input[i + j]))
                 .is_none()
         )
-        .unwrap()
-        .0 + window_size
+        .unwrap() + window_size
 }
 
 pub fn solve(input: &str) -> Solution {
-    let p1 = check(input, 4);
-    let p2 = check(input, 14);
+    let p1 = check(input.as_bytes(), 4);
+    let p2 = check(input.as_bytes(), 14);
     
     Solution::new(p1,p2)
 }
