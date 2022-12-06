8cc06dea21bb82136613c2c47a936dbe95469f85
Erik Aderstedt <erik@aderstedt.se>
2022-12-06 07:39:32 +0100

    Day 5 refactored

--- src/days_2022/day05.rs
+++ src/days_2022/day05.rs
@@ -5,39 +5,43 @@ use std::{str::FromStr, collections::VecDeque, str};
 
 const CRATE_WIDTH: usize = 4;
 
-#[derive(Debug)]
 struct Instruction {
     num_crates: usize,
     source: usize,
     dest: usize
 }
 
-fn top_of_stacks(stacks: &Vec<VecDeque<u8>>) -> String {
-    let tops: Vec<u8> = stacks.iter().map(|s| s.back().unwrap().clone()).collect();
-    str::from_utf8(&tops[..]).unwrap().to_string()
+#[derive(Clone)]
+struct CargoYard {
+    stacks: Vec<VecDeque<u8>>, 
 }
 
-fn simulate_with_cratemover_9000(moves: &Vec<Instruction>, stacks: &Vec<VecDeque<u8>>) -> String {
-    let mut stacks = stacks.clone();
+impl CargoYard {
 
-    for m in moves.iter() {
-        for _ in 0..m.num_crates {
-            let top_crate = stacks[m.source].pop_back().unwrap();
-            stacks[m.dest].push_back(top_crate);
+    fn top(&self) -> String {
+        let tops: Vec<u8> = self.stacks.iter().map(|s| s.back().unwrap().clone()).collect();
+        str::from_utf8(&tops[..]).unwrap().to_string()
+    }
+
+    fn simulate_with_cratemover_9000(&mut self, moves: &Vec<Instruction>) -> String {    
+        for m in moves.iter() {
+            for _ in 0..m.num_crates {
+                let top_crate = self.stacks[m.source].pop_back().unwrap();
+                self.stacks[m.dest].push_back(top_crate);
+            }
         }
+        self.top()
     }
 
-    top_of_stacks(&stacks)
-}
-
-fn simulate_with_cratemover_9001(moves: &Vec<Instruction>, stacks: &mut Vec<VecDeque<u8>>) -> String {
-    for m in moves.iter() {
-        let num_crates_in_source = stacks[m.source].len();
-        let mut p = stacks[m.source].split_off( num_crates_in_source - m.num_crates);
-        stacks[m.dest].append(&mut p);
+    fn simulate_with_cratemover_9001(&mut self, moves: &Vec<Instruction>) -> String {
+        for m in moves.iter() {
+            let num_crates_in_source = self.stacks[m.source].len();
+            let mut p = self.stacks[m.source].split_off( num_crates_in_source - m.num_crates);
+            self.stacks[m.dest].append(&mut p);
+        }
+    
+        self.top()
     }
-
-    top_of_stacks(stacks)
 }
 
 pub fn solve(input: &str) -> Solution {
@@ -49,27 +53,33 @@ pub fn solve(input: &str) -> Solution {
                 Err(s) => { println!("Error in line '{}': {}", m, s); None },
             })
         .collect();
+    let mut yard = input.parse::<CargoYard>().unwrap();
 
-    let mut stacks = Vec::with_capacity(10);
-    for line in input
-            .lines()
-            .take_while(|line| !line.starts_with(" 1")) {
-        let n = line.len() / CRATE_WIDTH + 1;
-        for i in 0..n {
-            let s: &str = &line[(i*CRATE_WIDTH)..];
-            if s.starts_with("[") {
-                while i >= stacks.len() {
-                    stacks.push(VecDeque::with_capacity(10));
+    Solution::new(yard.clone().simulate_with_cratemover_9000(&moves), 
+            yard.simulate_with_cratemover_9001(&moves))
+}
+
+impl FromStr for CargoYard {
+    type Err = &'static str;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let mut stacks = Vec::with_capacity(10);
+        for line in input
+                .lines()
+                .take_while(|line| !line.starts_with(" 1")) {
+            let n = line.len() / CRATE_WIDTH + 1;
+            for i in 0..n {
+                let s: &str = &line[(i*CRATE_WIDTH)..];
+                if s.starts_with("[") {
+                    while i >= stacks.len() {
+                        stacks.push(VecDeque::with_capacity(10));
+                    }
+                    stacks[i].push_front(s.as_bytes()[1])
                 }
-                stacks[i].push_front(s.as_bytes()[1])
             }
         }
+        Ok(CargoYard { stacks })
     }
-
-    let p1 = simulate_with_cratemover_9000(&moves, &stacks);
-    let p2 = simulate_with_cratemover_9001(&moves, &mut stacks);
-
-    Solution::new(p1, p2)
 }
 
 impl FromStr for Instruction {
