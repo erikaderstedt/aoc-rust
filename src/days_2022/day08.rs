df37484081bd35228f5bfb229a7464d05ac65175
Erik Aderstedt <erik@aderstedt.se>
2022-12-08 09:54:58 +0100

    Day 8 refactored

--- src/days_2022/day08.rs
+++ src/days_2022/day08.rs
@@ -3,35 +3,29 @@
 use crate::common::Solution;
 
 pub fn solve(input: &str) -> Solution {
-    let grid: Vec<Vec<u8>> = 
-    input.lines().map(|line| line.chars().map(|c| (c as u8)-('0' as u8)).collect()).collect();
+    let grid: Vec<&[u8]> = input.lines().map(|line| line.as_bytes()).collect();
     let w = grid[0].len();
     let h = grid.len();
 
     let p1: usize = w*h - (w-2)*(h-2) + (1..(h-1)).map(|y0|
-        (1..(w-1)).filter(|x0| {
-            let tree_height = grid[y0][*x0];
-            (0..*x0).find(|x| grid[y0][*x] >= tree_height).is_none() ||
-            ((x0+1)..w).find(|x| grid[y0][*x] >= tree_height).is_none() ||
-            (0..y0).find(|y| grid[*y][*x0] >= tree_height).is_none() ||
-            ((y0+1)..h).find(|y| grid[*y][*x0] >= tree_height).is_none()
+        (1..(w-1)).filter(|&x0| {
+            let tree_height = grid[y0][x0];
+            (0..x0).all(|x| grid[y0][x] < tree_height) ||
+            ((x0+1)..w).all(|x| grid[y0][x] < tree_height) ||
+            (0..y0).all(|y| grid[y][x0] < tree_height) ||
+            ((y0+1)..h).all(|y| grid[y][x0] < tree_height)
         }).count()
     ).sum::<usize>();
 
     let p2: usize = (1..(h-1)).map(|y0|
         (1..(w-1)).map(|x0| {
             let tree_height = grid[y0][x0];
-            let up = (0..y0).rev().take_while(|y| grid[*y][x0] < tree_height).count();
-            let down = ((y0+1)..h).take_while(|y| grid[*y][x0] < tree_height).count();
-            let left = (0..x0).rev().take_while(|x| grid[y0][*x] < tree_height).count();
-            let right = ((x0+1)..w).take_while(|x| grid[y0][*x] < tree_height).count();
-            (if left == x0 { left } else { left  + 1 }) *
-            (if up == y0 { up } else { up  + 1 }) *
-            (if right == (w-1-x0) { right } else { right  + 1 }) *
-            (if down == (h-1 - y0) { down } else { down  + 1 })
-
-        }).max().unwrap()
-    ).max().unwrap();
+            (match ((y0+1)..h).find(|&y| grid[y][x0] >= tree_height) { Some(y) => y-y0, None => h-y0-1 }) *
+            (match (1..y0).find(|&y| grid[y0-y][x0] >= tree_height) { Some(y) => y, None => y0 }) *
+            (match (1..x0).find(|&x| grid[y0][x0-x] >= tree_height) { Some(x) => x, None => x0 }) *
+            (match ((x0+1)..w).find(|&x| grid[y0][x] >= tree_height) { Some(x) => x-x0, None => w-x0-1 })
+        }).max().unwrap_or(0)
+    ).max().unwrap_or(0);
 
     Solution::new(p1,p2)
 }
