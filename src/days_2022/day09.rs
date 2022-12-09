7f60618d275236170462d6afdec532658cb3b481
Erik Aderstedt <erik@aderstedt.se>
2022-12-09 15:19:57 +0100

    Day 9 refactored again

--- src/days_2022/day09.rs
+++ src/days_2022/day09.rs
@@ -1,44 +1,23 @@
 // https://adventofcode.com/2022/day/9
 
 use crate::common::Solution;
-use std::collections::{HashSet, hash_set};
 use std::str::FromStr;
+use std::collections::HashSet;
 use std::hash::Hash;
 
 #[derive(Eq,PartialEq,Clone,Copy,Hash)]
-struct Knot { x: i16, y: i16 }
+struct KnotPosition { x: i16, y: i16 }
 enum Direction { Up, Down, Left, Right }
 struct Instruction { direction: Direction, steps: usize }
 
-const STARTING_POINT: Knot = Knot { x: 0, y: 0};
-
-// This wraps HashSet to remember the last inserted item.
-// This will drastically improve perfomance in cases where 
-// we often insert the same item as the previously inserted item.
-struct LastCheckingHashSet<T: Hash + Eq + Copy> {
-    hash_set: HashSet<T>,
-    last: T
-}
-
-impl<T> LastCheckingHashSet<T> where T: Hash + Eq + Copy {
-    fn new(item: T) -> Self {
-        Self { hash_set: HashSet::from([item]), last: item }
-    }
-
-    fn insert(&mut self, item: T) {
-        if self.last != item {
-            self.last = item;
-            self.hash_set.insert(item);
-        }
-    } 
-
-    fn len(&self) -> usize { self.hash_set.len() }
-}
+const STARTING_POINT: KnotPosition = KnotPosition { x: 0, y: 0};
 
 pub fn solve(input: &str) -> Solution {
     let mut knots = [STARTING_POINT; 10];
-    let mut visited_p1 = LastCheckingHashSet::new(STARTING_POINT);
-    let mut visited_p2 = LastCheckingHashSet::new(STARTING_POINT);
+    let mut visited_p1: HashSet<KnotPosition> = HashSet::new();
+    let mut visited_p2: HashSet<KnotPosition> = HashSet::new();
+    visited_p1.insert(STARTING_POINT);
+    visited_p2.insert(STARTING_POINT);
     
     for line in input.lines() {
         let instruction: Instruction = line.parse().expect("Unable to parse instruction");
@@ -49,22 +28,25 @@ pub fn solve(input: &str) -> Solution {
                 Direction::Left => knots[0].x -= 1,
                 Direction::Right => knots[0].x += 1,
             };
-    
-            for i in 1..10 {
-                let diff_x = knots[i-1].x - knots[i].x;
-                let diff_y = knots[i-1].y - knots[i].y;
-                if diff_x.abs() > 1 || diff_y.abs() > 1 {
-                    knots[i].x += diff_x.signum();
-                    knots[i].y += diff_y.signum();
+            if let Some(num_iterations) = (1..10)
+                .take_while(|&i| {
+                    let diff_x = knots[i-1].x - knots[i].x;
+                    let diff_y = knots[i-1].y - knots[i].y;
+                    if diff_x.abs() > 1 || diff_y.abs() > 1 {
+                        knots[i].x += diff_x.signum();
+                        knots[i].y += diff_y.signum();
+                        true
+                    } else { false }})
+                .last() {
+                visited_p1.insert(knots[2-1]);
+                if num_iterations == 9 {
+                    visited_p2.insert(knots[10-1]);
                 }
             }
-            visited_p1.insert(knots[2-1]);
-            visited_p2.insert(knots[10-1]);
         }
     }
     
     Solution::new(visited_p1.len(), visited_p2.len())
-
 }
 
 impl FromStr for Instruction {
