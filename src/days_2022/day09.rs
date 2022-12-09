29e4370f50062e5470beb4d9cc2804854a4c83bd
Erik Aderstedt <erik@aderstedt.se>
2022-12-09 07:39:36 +0100

    Day 9

--- src/days_2022/day09.rs
+++ src/days_2022/day09.rs
@@ -1,96 +1,99 @@
 // https://adventofcode.com/2022/day/9
 
-use crate::common::Solution;
+use crate::common::{Solution, parsed_from_each_line};
 use std::collections::HashSet;
-use std::hash::{Hash, Hasher};
+use std::hash::Hash;
+use std::str::FromStr;
 
-#[derive(Debug,Eq,PartialEq,Copy,Clone)]
+const N: usize = 10;
+
+#[derive(Debug,Eq,PartialEq,Copy,Clone,Hash)]
 struct Position {
     x: i32,
     y: i32,
 }
 
-impl Position {
+enum Direction { Up, Down, Left, Right }
 
-    fn touching(&self, other: &Position) -> bool {
-        let diff_x = self.x - other.x;
-        let diff_y = self.y - other.y;
-        diff_x.abs() <= 1 && diff_y.abs() <= 1
-    }
-
-}
-
-impl Hash for Position {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.x.hash(state);
-        self.y.hash(state);
-    }
+struct Instruction {
+    direction: Direction,
+    steps: usize,
 }
 
 #[derive(Debug)]
 
-struct State {
-    head: Position,
-    tail: Position,
+struct Rope<const N: usize> {
+    knots: [Position;N]
 }
 
-impl State {
-    fn new() -> State {
-        State { head: Position { x: 0, y: 0 }, tail: Position { x: 0, y: 0}}
+impl Rope<N> {
+    fn new() -> Rope<N> {
+        Rope { knots: [Position { x: 0, y: 0 }; N] }
     }
 
-    fn step(&mut self, direction: &str) {
+    fn step(&mut self, direction: &Direction) {
         // Move head
         match direction {
-            "U" => self.head.y += 1,
-            "D" => self.head.y -= 1,
-            "L" => self.head.x -= 1,
-            "R" => self.head.x += 1,
-            _ => println!("WARNING: unrecognized direction '{}'.", direction),
+            Direction::Up => self.knots[0].y += 1,
+            Direction::Down => self.knots[0].y -= 1,
+            Direction::Left => self.knots[0].x -= 1,
+            Direction::Right => self.knots[0].x += 1,
         };
 
-
-        if !self.head.touching(&self.tail) {
-            let diff_x = self.head.x - self.tail.x;
-            let diff_y = self.head.y - self.tail.y;
-            if diff_y > 1 {
-                self.tail.y +=1;
-                self.tail.x = self.head.x;
-            } else if diff_y < -1 {
-                self.tail.y -=1;
-                self.tail.x = self.head.x;
-            } else if diff_x > 1 {
-                self.tail.x += 1;                    
-                self.tail.y = self.head.y;
-            } else if diff_x < -1 {
-                    self.tail.x -= 1;                    
-                self.tail.y = self.head.y;
+        for i in 1..N {
+            let diff_x = self.knots[i-1].x - self.knots[i].x;
+            let diff_y = self.knots[i-1].y - self.knots[i].y;
+            let touching = diff_x.abs() <= 1 && diff_y.abs() <= 1;
+            
+            if !touching {
+                self.knots[i].x += diff_x.signum();
+                self.knots[i].y += diff_y.signum();
             }
+
         }
+    }
+}
 
-        println!("Move: '{}': State {:?}", direction, self);
+fn follow_instructions<const N: usize>(instructions: &Vec<Instruction>) -> usize {
+    let mut rope = Rope::new();
+    let mut visited: HashSet<Position> = HashSet::new();
 
-    } 
-    
+    for instruction in instructions.iter() {
+        for _ in 0..instruction.steps {
+            rope.step(&instruction.direction);
+            visited.insert(rope.knots[N-1]);
+        }
+    }
+
+    visited.len()
 }
 
 pub fn solve(input: &str) -> Solution {
+    let instructions: Vec<Instruction> = parsed_from_each_line(input);
+    let p1 = follow_instructions::<2>(&instructions);
+    let p2 = follow_instructions::<10>(&instructions);
 
-    let mut state = State::new();
-    let mut visited: HashSet<Position> = HashSet::new();
-
-    for line in input.lines() {
-        if let Some((direction, num_steps)) = line.split_once(" ") {
-            let num_steps = num_steps.parse::<usize>().unwrap();
-            for _ in 0..num_steps {
-                state.step(direction);
-                visited.insert(state.tail);
-            }
-        }
-    }
-
-    let p1 = visited.len();
-
-    Solution::new(p1,0)
+    Solution::new(p1,p2)
 }
 
+impl FromStr for Instruction {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+
+        match s.split_once(' ') {            
+            Some((dir, n)) => {
+                let direction = match dir.as_bytes()[0] as char {
+                    'U' => Ok(Direction::Up),
+                    'L' => Ok(Direction::Left),
+                   'R' => Ok(Direction::Right),
+                    'D' => Ok(Direction::Down),
+                    _ => Err("Invalid direction value"),
+                }?;
+                let steps = n.parse::<usize>().map_err(|_| "Invalid num_steps value.")?;
+                Ok(Instruction { direction, steps })
+            },
+            _ => Err("Malformed line."),
+        }
+    }
+}
