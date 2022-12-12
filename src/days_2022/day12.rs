aba3334e3e0f6f625d008fe9ffca81cb78470b3a
Erik Aderstedt <erik.aderstedt@hasselblad.com>
2022-12-12 09:34:36 +0100

    Day 12 refactored

--- src/days_2022/day12.rs
+++ src/days_2022/day12.rs
@@ -9,7 +9,8 @@ use std::fmt;
 
 const START: u8 = 'S' as u8;
 const END: u8 = 'E' as u8;
-
+const LOWEST: u8 = 'a' as u8;
+const HIGHEST: u8 = 'z' as u8;
 
 #[derive(Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Pos {
@@ -27,7 +28,7 @@ impl Pos {
     fn from(x: usize, y: usize) -> Pos { Pos { x, y }}
 }
 
-fn find_path<FS>(dtm: &Vec<Vec<u8>>, start: &Pos, end: FS) -> Option<usize>
+fn find_path<FS>(dtm: &Vec<Vec<u8>>, start: &Pos, end: FS, climbing: bool) -> Option<usize>
 where
     FS: FnMut(&Pos) -> bool,
 {
@@ -37,20 +38,20 @@ where
     bfs(start, |p| {
         let mut v: Vec<Pos> = Vec::with_capacity(4);
         let compare = match dtm[p.y][p.x] {
-            START => 'a' as u8,
-            END => 'z' as u8,
+            START => LOWEST,
+            END => HIGHEST,
             i => i,
-        } + 1;
-        if p.x > 0 && dtm[p.y][p.x-1] <= compare {
+        };
+        if p.x > 0 && ((climbing && dtm[p.y][p.x-1] <= compare + 1) || (!climbing && dtm[p.y][p.x-1] >= compare - 1)) {
             v.push(Pos { x: p.x - 1, y: p.y})
         }
-        if p.x < width - 1 && dtm[p.y][p.x+1] <= compare {
+        if p.x < width - 1 && ((climbing && dtm[p.y][p.x+1] <= compare + 1) || (!climbing && dtm[p.y][p.x+1] >= compare - 1)) {
             v.push(Pos { x: p.x + 1, y: p.y})
         }
-        if p.y > 0 && dtm[p.y-1][p.x] <= compare {
+        if p.y > 0 && ((climbing && dtm[p.y-1][p.x] <= compare + 1) || (!climbing && dtm[p.y-1][p.x] >= compare - 1)) {
             v.push(Pos { x: p.x, y: p.y - 1})
         }
-        if p.y < height - 1 && dtm[p.y+1][p.x] <= compare {
+        if p.y < height - 1 && ((climbing && dtm[p.y+1][p.x] <= compare + 1) || (!climbing && dtm[p.y+1][p.x] >= compare - 1)) {
             v.push(Pos { x: p.x, y: p.y + 1})
         }
         v
@@ -62,30 +63,19 @@ pub fn solve(input: &str) -> Solution {
         .map(|line| line.as_bytes().to_vec())
         .collect();
 
-    let x = dtm.iter().find_map(|line| line.iter().find_position(|&&c| c == 'S' as u8)).unwrap().0;
-    let y = dtm.iter().find_position(|line| line.contains(&('S' as u8))).unwrap().0;
-    let target_x = dtm.iter().find_map(|line| line.iter().find_position(|&&c| c == 'E' as u8)).unwrap().0;
-    let target_y = dtm.iter().find_position(|line| line.contains(&('E' as u8))).unwrap().0;
-
-    // // Pre-build a hashmap of Position -> vec![Position]
-    // let m: HashMap<Pos, Vec<Pos>> = (0..height)
-    //     .zip(0..width)
-    //     .map(|(y,x)| {
-    //         let mut v: Vec<Pos> = Vec::with_capacity(4);
-    //         if x > 0 && dtm[y][x-1] <= dtm[y][x] { v.push(Pos::from(x-1, y)) }
-    //         if x < width - 1  && dtm[y][x+1] <= dtm[y][x] { v.push(Pos::from(x+1, y)) }
-    //         if y > 0 && dtm[y-1][x] <= dtm[y][x] { v.push(Pos::from(x, y-1)) }
-    //         if y < height - 1 && dtm[y+1][x] <= dtm[y][x] { v.push(Pos::from(x, y+1)) }
-    //         (Pos::from(x, y), v)
-    //     })
-    //     .collect();
+    let x = dtm.iter().find_map(|line| line.iter().find_position(|&&c| c == START)).unwrap().0;
+    let y = dtm.iter().find_position(|line| line.contains(&START)).unwrap().0;
+    let target_x = dtm.iter().find_map(|line| line.iter().find_position(|&&c| c == END)).unwrap().0;
+    let target_y = dtm.iter().find_position(|line| line.contains(&END)).unwrap().0;
 
     let starting_pos = Pos::from(x, y);            
     let goal_pos = Pos { x: target_x, y: target_y };
-    let p1 = find_path(&dtm, &starting_pos, |p| *p == goal_pos).expect("Unable to reach E");
-    let p2 = find_path(&dtm, &goal_pos, |p| dtm[p.y][p.x] == ('a' as u8) || dtm[p.y][p.x] == START).expect("Unable to reach any 'a'.");
+    let p1 = find_path(&dtm, &starting_pos, |p| *p == goal_pos, true).expect("Unable to reach E");
+    let p2 = find_path(&dtm, &goal_pos, |p| 
+        match dtm[p.y][p.x] {
+            START | LOWEST => true,
+            _ => false,
+        }, false).expect("Unable to reach any 'a'.");
     
-
-
     Solution::new(p1,p2)
 }
