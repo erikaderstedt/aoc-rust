52c30fc684849d01a304188dd7b08e2b698f7779
Erik Aderstedt <erik.aderstedt@hasselblad.com>
2022-12-14 09:07:56 +0100

    Day 14 array instead of Grid

--- src/days_2022/day14.rs
+++ src/days_2022/day14.rs
@@ -1,60 +1,66 @@
 // https://adventofcode.com/2022/day/14
 use crate::common::Solution;
-use crate::grid::{Grid,GridElement, Position};
 
 const X_OFFSET: usize = 300;
 const HEIGHT: usize = 170;
 const WIDTH: usize = 400;
 
-fn introduce_sand(cave: &mut Grid<CaveItem>, at: Position) -> bool {
-    let mut p = at;
+// const X_OFFSET: usize = 480;
+// const HEIGHT: usize = 20;
+// const WIDTH: usize = 40;
 
+const START_X: usize = 500 - X_OFFSET;
+const START_Y: usize = 0;
+
+type Cave = [u8; WIDTH*HEIGHT];
+const AIR: u8 = 0;
+const SAND: u8 = 1;
+const ROCK: u8 = 2;
+
+fn introduce_sand(cave: &mut Cave, mut x: usize, mut y: usize) -> bool {
     loop { 
-        if p.row == HEIGHT - 1 {
+        y += 1;
+        if y == HEIGHT {
             return false;
         }
-        assert!(p.column < WIDTH - 1);
-        assert!(p.column > 0);
         
-        p.row += 1;
-        if cave[&p] != CaveItem::Air {
-            p.column -= 1;
-            if cave[&p] != CaveItem::Air {
-                p.column += 2;
-                if cave[&p] != CaveItem::Air {
-                    p.column -= 1; p.row -= 1;
-                    cave[&p] = CaveItem::Sand;
-                    return true;
+        if cave[y*WIDTH + x] != AIR {
+            if cave[y*WIDTH + x - 1] != AIR {
+                if cave[y*WIDTH + x + 1] != AIR {
+                    cave[(y-1)*WIDTH + x] = SAND;
+                    return true
+                } else {
+                    x += 1;
                 }
+            } else {
+                x -= 1;
             }
         }
     }
 }
 
-fn add_sand_until_it_falls_off(cave: &mut Grid<CaveItem>) -> usize {
+fn add_sand_until_it_falls_off(cave: &mut [u8; WIDTH*HEIGHT]) -> usize {
     let mut p1 = 0;
-    let start = Position { column: 500 - X_OFFSET, row: 0 };
-    while introduce_sand(cave, start.clone()) {
+    while introduce_sand(cave, START_X, START_Y) {
         p1 += 1;
     }
     p1
 }
 
-fn add_sand_until_full(cave: &mut Grid<CaveItem>) -> usize {
+fn add_sand_until_full(cave: &mut [u8; WIDTH*HEIGHT]) -> usize {
     let mut p2 = 0;
-    let start = Position { column: 500 - X_OFFSET, row: 0 };
-    while cave[&start] == CaveItem::Air && introduce_sand(cave, start.clone()) {
+    while cave[START_Y*WIDTH + START_X] == AIR && introduce_sand(cave, START_X, START_Y) {
         p2 += 1;
     }
     p2
 }
 
 pub fn solve(input: &str) -> Solution {
-    let mut cave = Grid { rows: HEIGHT, cols: WIDTH, locations: vec![CaveItem::Air; WIDTH * HEIGHT] };
+    let mut cave: Cave = [AIR; WIDTH*HEIGHT];
 
     let mut bottom = 0;
     for line in input.lines() {
-        let groups: Vec<Position> = line.split(" -> ").filter_map(|g| g
+        let groups: Vec<(usize,usize)> = line.split(" -> ").filter_map(|g| g
                 .split_once(",")
                 .map(|(s1,s2)| {
                     let n1 = s1.parse::<usize>().unwrap();
@@ -62,55 +68,41 @@ pub fn solve(input: &str) -> Solution {
                     assert!(n1 >= X_OFFSET, "Column too far to the left");
                     assert!(n1 < X_OFFSET + WIDTH, "Too narrow");
                     assert!(n2 < HEIGHT, "Too low");
-                    Position { column: n1 - X_OFFSET, row: n2 }
+                    (n1 - X_OFFSET, n2)
                 }))    
                 .collect();
 
-        bottom = bottom.max(groups.iter().map(|g| g.row).max().unwrap());
+        bottom = bottom.max(groups.iter().map(|g| g.1).max().unwrap());
 
         for g in groups.windows(2) {
-            for row in g[0].row.min(g[1].row)..=g[0].row.max(g[1].row) {
-                for column in g[0].column.min(g[1].column)..=g[0].column.max(g[1].column) {
-                    cave[& Position { column, row }] = CaveItem::Rock;
+            for row in g[0].1.min(g[1].1)..=g[0].1.max(g[1].1) {
+                for column in g[0].0.min(g[1].0)..=g[0].0.max(g[1].0) {
+                    cave[row*WIDTH + column] = ROCK;
                 }
             }
         }
     }
 
+    // for y in 0..HEIGHT {
+    //     for x in 0..WIDTH {
+    //         print!("{}",
+    //         match cave[y*WIDTH + x] {
+    //             AIR => '.',
+    //             SAND => 'o',
+    //             ROCK => '#',
+    //             _ => '?',
+    //         });
+    //     }
+    //     println!("");
+    // }
+
     let p1 = add_sand_until_it_falls_off(&mut cave);
 
     for x in 0..WIDTH {
-        let p = Position { column: x, row: bottom + 2 };
-        cave[&p] = CaveItem::Rock;
+        cave[(bottom + 2)*WIDTH + x] = ROCK;
     }
 
     let p2 = add_sand_until_full(&mut cave) + p1;
 
     Solution::new(p1,p2)
-}
-
-#[derive(Clone,PartialEq,Eq)]
-enum CaveItem {
-    Air,
-    Rock,
-    Sand,
-}
-
-impl GridElement for CaveItem {
-    fn from_char(c: &char) -> Option<Self> {
-        match c {
-            '#' => Some(CaveItem::Rock),
-            'o' => Some(CaveItem::Sand),
-            '.' => Some(CaveItem::Air),
-            _ => None,
-        }
-    }
-
-    fn to_char(&self) -> char {
-        match self {
-            CaveItem::Air => '.',
-            CaveItem::Rock => '#',
-            CaveItem::Sand => 'o',
-        }
-    }    
 }
