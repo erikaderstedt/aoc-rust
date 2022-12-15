523fa0c529213aeab5e64d21685e73ae5c816362
Erik Aderstedt <erik.aderstedt@hasselblad.com>
2022-12-15 11:34:19 +0100

    Day 14 refactored pt1

--- src/days_2022/day15.rs
+++ src/days_2022/day15.rs
@@ -1,25 +1,26 @@
 // https://adventofcode.com/2022/day/15
 use crate::common::{Solution, parsed_from_each_line};
 use std::str::{FromStr, from_utf8};
-use std::hash::Hash;
-use std::collections::HashSet;
-
-#[derive(Debug)]
-struct Sensor {
-    x: isize,
-    y: isize,
-    range: usize,
-}
-
-type Position = (isize,isize);
-
-#[derive(Debug,Hash,Eq,PartialEq,Clone)]
-struct Beacon {
-    x: isize,
-    y: isize,
-}
+use itertools::Itertools;
+use std::ops::Range;
+use std::collections::VecDeque;
+use std::time::Instant;
+struct Beacon { x: isize, y: isize }
+struct Sensor { x: isize, y: isize, range: usize, }
 
 impl Sensor {
+
+    fn range_at_row(&self, row: isize) -> Option<Range<isize>> {
+        let reach = (self.range - row.abs_diff(self.y)) as isize;
+        if reach >= 0 {
+            let length = 2 * (reach + 1) - 1;
+            let start = self.x-reach;
+            let end = start + length;
+            Some(start..end)
+        } else {
+            None
+        }
+    }
 
     fn covers(&self, x: isize, y: isize) -> bool {
         self.x.abs_diff(x) + self.y.abs_diff(y) <= self.range
@@ -28,40 +29,74 @@ impl Sensor {
     fn iterate_over_outside_edge(&self) -> EdgeIterator {
         EdgeIterator { sensor_x: self.x, sensor_y: self.y, edge: Edge::TopLeft, done: false, x: self.x, y: self.y - (self.range as isize) - 1 }
     }
-}
+} 
 
 const P1_ROW: isize = 2000000;
+// const P1_ROW: isize = 10;
+
 const P2_Y_MAX: isize = 4000000;
 const P2_X_MAX: isize = 4000000;
 
+fn ranges_at_y(sensors: &Vec<Sensor>, y: isize) -> Vec<Range<isize>> {
+    let mut ranges: VecDeque<Range<isize>> = sensors
+        .iter()
+        .filter_map(|sensor| sensor.range_at_row(y))
+        .sorted_by_key(|range| range.start)
+        .collect();
+
+    match ranges.pop_front() {
+        Some(mut current) => {
+            let mut r = vec![];
+            loop {
+                if let Some(next) = ranges.pop_front() {
+                    if next.start <= current.end {
+                        current.end = next.end.max(current.end);
+                    } else {
+                        r.push(current);
+                        current = next;
+                    }
+                } else {
+                    r.push(current);
+                    break r
+                }
+            }
+        },
+        None => vec![],
+    }
+
+}
+
 pub fn solve(input: &str) -> Solution {
     let sensors: Vec<Sensor> = parsed_from_each_line(input);
-    let beacons: HashSet<Beacon> = parsed_from_each_line(input).into_iter().collect();
-
-    let min_x = sensors.iter().map(|s| s.x - (s.range as isize)).min().unwrap();
-    let max_x = sensors.iter().map(|s| s.x + (s.range as isize)).max().unwrap();
+    let num_beacons_at_p1_row = parsed_from_each_line::<Beacon>(input)
+        .into_iter()
+        .filter(|beacon| beacon.y == P1_ROW)
+        .map(|beacon| beacon.x)
+        .sorted()
+        .dedup()
+        .count();
 
     let mut p1 = 0;
-    for x in (min_x-1)..=(max_x+1) {
-        let a = sensors.iter().any(|s| s.covers(x, P1_ROW));        
-        if a {
-            p1 += 1;
-        }
-    }
-    p1 -= beacons.iter().filter(|b| b.y == P1_ROW).count();
+
+    let start = Instant::now();        
+
+    let p1 = ranges_at_y(&sensors, P1_ROW).into_iter().map(|range| range.len()).sum::<usize>() - num_beacons_at_p1_row;
+
+    let duration = start.elapsed();
+
+    println!("P1 Elapsed time: {:>7} µs", duration.as_micros());
+
+    // let p2 = (0..P2_Y_MAX).find(|&y| ranges_at_y(&sensors, y).len() > 1).unwrap();
 
     let p2 = sensors.iter().find_map(|sensor|
         match sensor
                 .iterate_over_outside_edge()
                 .filter(|(x,y)| *x >= 0 && *x <= P2_X_MAX && *y >= 0 && *y <= P2_Y_MAX)
-                .find(|p| !sensors
+                .find(|(x, y)| !sensors
                             .iter()
-                            .any(|s| s.covers(p.0,p.1))
+                            .any(|s| s.covers(*x,*y))
                 ) {
-            Some((x,y)) => {
-                let p2 = x * 4000000 + y;
-                Some(p2)
-            },
+            Some((x,y)) => { println!("{} {}", x, y); Some(x * 4000000 + y) },
             None => None,
         }).unwrap();
 
@@ -111,7 +146,7 @@ struct EdgeIterator {
 }
 
 impl Iterator for EdgeIterator {
-    type Item = Position;
+    type Item = (isize,isize);
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.done { return None }
