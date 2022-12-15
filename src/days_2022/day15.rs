99557f8749866158f30458d3c9fe7f9934fc75dd
Erik Aderstedt <erik@aderstedt.se>
2022-12-15 14:13:53 +0100

    Day 15 refactored again

--- src/days_2022/day15.rs
+++ src/days_2022/day15.rs
@@ -4,8 +4,7 @@ use std::str::{FromStr, from_utf8};
 use itertools::Itertools;
 use std::ops::Range;
 
-struct Beacon { x: isize, y: isize }
-struct Sensor { x: isize, y: isize, range: usize, }
+struct Sensor { x: isize, y: isize, beacon: (isize, isize), range: usize, }
 
 impl Sensor {
 
@@ -21,13 +20,23 @@ impl Sensor {
         }
     }
 
-    fn zero_intersects(&self) -> Vec<isize> {
+    fn zero_intersects_negative_slope(&self) -> Vec<isize> {
         let r = self.range as isize;
         vec![
-            self.x + (self.y + r), // bottom right edge
-            self.x + (self.y - r), // top left edge
-            self.x - (self.y - r), // top right edge
-            self.x - (self.y + r), // bottom left edge
+            self.x - (self.y - r) - 1, // top right edge
+            self.x - (self.y + r) - 1, // bottom left edge
+            self.x - (self.y - r) + 1, // top right edge
+            self.x - (self.y + r) + 1, // bottom left edge
+        ]
+    }
+
+    fn zero_intersects_positive_slope(&self) -> Vec<isize> {
+        let r = self.range as isize;
+        vec![
+            self.x + (self.y + r) + 1, // bottom right edge
+            self.x + (self.y + r) - 1, // bottom right edge
+            self.x + (self.y - r) - 1, // top left edge
+            self.x + (self.y - r) - 1, // top left edge
         ]
     }
 
@@ -67,41 +76,47 @@ fn ranges_at_y(sensors: &Vec<Sensor>, y: isize) -> Vec<Range<isize>> {
         },
         None => vec![],
     }
-
 }
 
 pub fn solve(input: &str) -> Solution {
     let sensors: Vec<Sensor> = parsed_from_each_line(input);
-    let num_beacons_at_p1_row = parsed_from_each_line::<Beacon>(input)
-        .into_iter()
-        .filter(|beacon| beacon.y == P1_ROW)
-        .map(|beacon| beacon.x)
+    let num_beacons_at_p1_row = sensors.iter()
+        .filter_map(|sensor| 
+            if sensor.beacon.1 == P1_ROW {
+                Some(sensor.beacon.0)
+            } else {
+                None
+            })
         .sorted()
         .dedup()
         .count();
 
     let p1 = ranges_at_y(&sensors, P1_ROW).into_iter().map(|range| range.len()).sum::<usize>() - num_beacons_at_p1_row;
 
-    let i: Vec<isize> = 
-    sensors
+    let x_axis_locations_positive: Vec<isize> = sensors
         .iter()
-        .map(|sensor| sensor.zero_intersects().into_iter())
-        .flatten()
-        .map(|v| vec![v-1, v+1].into_iter())
+        .map(|sensor| sensor.zero_intersects_positive_slope().into_iter())
         .flatten()
         .sorted()
+        .dedup()
         .collect();
 
-    let p2 = iproduct!(i.iter(), i.iter())
+    let x_axis_locations_negative: Vec<isize> = sensors
+        .iter()
+        .map(|sensor| sensor.zero_intersects_negative_slope().into_iter())
+        .flatten()
+        .sorted()
+        .dedup()
+        .collect();
+    
+    let p2 = iproduct!(x_axis_locations_negative.iter(), x_axis_locations_positive.iter())
         .find_map(|(x1, x2)| {
             let d = x1.abs_diff(*x2) as isize;
-            if d % 2 != 0 {
+            if d % 2 != 0 || x2 < x1 {
                 None
             } else {
                 let x = d / 2 + x1.min(x2);
-                let y = d / 2 * (x1-x2).signum();
-
-                // println!("Checking {} {}", x, y);
+                let y = d / 2 * (x2-x1).signum();
                 if y >= 0 && y <= P2_Y_MAX && x >= 0 && x <= P2_X_MAX && sensors.iter().all(|sensor| !sensor.covers(x, y)) {
                     Some(x * 4000000 + y)
                 } else {
@@ -124,17 +139,7 @@ impl FromStr for Sensor {
         let closest_beacon_x = from_utf8(&w[8].as_bytes()[2..(w[8].as_bytes().len()-1)]).unwrap().parse::<isize>().unwrap();
         let closest_beacon_y = from_utf8(&w[9].as_bytes()[2..(w[9].as_bytes().len()-0)]).unwrap().parse::<isize>().unwrap();
         let range = x.abs_diff(closest_beacon_x) + y.abs_diff(closest_beacon_y);
-        Ok(Sensor { x, y, range })
+        Ok(Sensor { x, y, beacon: (closest_beacon_x, closest_beacon_y), range })
     }
 }
 
-impl FromStr for Beacon {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let w: Vec<&str> = s.split(" ").collect();
-        let x = from_utf8(&w[8].as_bytes()[2..(w[8].as_bytes().len()-1)]).unwrap().parse::<isize>().unwrap();
-        let y = from_utf8(&w[9].as_bytes()[2..(w[9].as_bytes().len()-0)]).unwrap().parse::<isize>().unwrap();
-        Ok(Beacon { x,y })
-    }
-}
