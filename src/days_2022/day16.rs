fd1989d86d7d9ab4bf55c4e48cf8f1f782ef833e
Erik Aderstedt <erik.aderstedt@hasselblad.com>
2022-12-16 18:17:06 +0100

    Day 16 pt 1 refactored.

--- src/days_2022/day16.rs
+++ src/days_2022/day16.rs
@@ -1,260 +1,205 @@
-use itertools::Itertools;
 
 // https://adventofcode.com/2022/day/16
-use crate::common::{Solution, parsed_from_each_line};
-use std::str::FromStr;
+use crate::common::Solution;
 use std::collections::HashMap;
-use std::hash::Hash;
+use itertools::Itertools;
+use pathfinding::prelude::bfs;
+use std::cmp::Ordering;
+use std::str::FromStr;
+
+type LocationIndex = u8;
+
+#[derive(Debug)]
+struct Tunnel {
+    destination: LocationIndex,
+    length: u8,
+}
 
 #[derive(Debug)]
 struct Room {
-    name: String,
     flow_rate: usize,
-    tunnels_to_rooms: Vec<String>,
-}
-impl FromStr for Room {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let name = s[6..8].to_string();
-        let flow_rate = s[23..].split(";").next().unwrap().parse::<usize>().unwrap();
-        let tunnels_to_rooms = if s.contains("valves") {
-            s.split("valves ").skip(1).next().unwrap().split(", ").map(|s| s.to_string()).collect()
-        } else {
-            let valve = s.split("valve ").skip(1).next().unwrap().to_string();
-            vec![valve]
-        };
-        Ok(Room { name, flow_rate, tunnels_to_rooms })
-    }
+    tunnels: Vec<Tunnel>,
 }
 
-#[derive(Debug,Hash,PartialEq, Eq)]
-struct State {
-    location: String,
-    flow_rate: usize,
-    open_valves: Vec<String>,
-    // minutes_left: usize,
+#[derive(Debug,Clone,PartialEq,Eq,PartialOrd,Ord)]
+struct SingleOperatorStateInPlay {
+    location: LocationIndex,
+    open_valves: u64,
+    time_expended: usize,
     total_released_pressure: usize
 }
 
-impl State {
-    fn new() -> State { State { location: "AA".to_string(), open_valves: vec![], flow_rate: 0, 
-    // minutes_left: 30, 
-    total_released_pressure: 0 }}
+impl SingleOperatorStateInPlay {
 
-    fn all_valves_are_open(&self, rooms: &HashMap<String,Room>) -> bool {
-        rooms.iter().all(|room| room.1.flow_rate == 0 || self.open_valves.contains(room.0))
-    }
+    fn valve_open(&self, valve: &LocationIndex) -> bool { (self.open_valves >> (*valve as u64)) & 1 == 1 }
 
-    fn successor_states(&self, rooms: &HashMap<String,Room>) -> Vec<State> {
-        // if self.minutes_left == 0 { return vec![] };
-
-        let mut v = vec![];
-        let location = rooms.get(&self.location).unwrap();
-        if location.flow_rate > 0 && !self.open_valves.contains(&self.location) {
-            let mut valves = self.open_valves.clone();
-            valves.push(self.location.clone());
-            v.push(State { 
-                location: self.location.clone(),
-                flow_rate: self.flow_rate + location.flow_rate,
-                // minutes_left: self.minutes_left - 1,
-                open_valves: valves,
-                total_released_pressure: self.total_released_pressure + self.flow_rate,  
-            })
-        }
-        for tunnel in location.tunnels_to_rooms.iter() {
-            v.push(State { 
-                location: tunnel.clone(), 
-                flow_rate: self.flow_rate, 
-                open_valves: self.open_valves.clone(), 
-                // minutes_left: self.minutes_left - 1, 
-                total_released_pressure: self.total_released_pressure + self.flow_rate, 
-            })
-        }
-        v
-    }
-
-    fn move_through(rooms: &HashMap<String,Room>) -> usize {
-
-        let mut states: Vec<State> = vec![State::new()];
-        for _minute in 1..=30 {
-            // Get all possible states
-            // Replace all states by their successor states.
-            states = states.into_iter()
-                .map(|state| state.successor_states(rooms).into_iter())
-                .flatten()
-                .sorted_by_key(|state| state.total_released_pressure)
-                .rev()
-                .take(400)
-                .collect();
-
-            // if states.iter().any(|state| state.all_valves_are_open(rooms)) {
-            //     states = states.into_iter().filter(|state| state.all_valves_are_open(rooms)).collect();
-            // }
-    
-            // println!("Minute: {}, States: {:?}", minute, states);
-        }
-    
-        let best = states.into_iter().max_by_key(|state| state.total_released_pressure).unwrap();
-        println!("Best state;: {:?}", best);
-        best.total_released_pressure
+    fn all_valves_are_open(&self) -> bool {
+        self.open_valves == u64::MAX
     }
 }
 
+#[derive(Debug,Clone,PartialEq,Eq,Ord)]
 
-#[derive(Debug,Hash,PartialEq, Eq,Clone)]
-struct ElephantHelperState {
-    location: String,
-    elephant_location: String,
-    flow_rate: usize,
-    open_valves: Vec<String>,
-    last_move_was_to_open_current_valve: bool,
-    // minutes_left: usize,
-    total_released_pressure: usize
+enum SingleOperatorState {
+    Ongoing(SingleOperatorStateInPlay),
+    Finished(usize)
 }
 
-impl ElephantHelperState {
-    fn new() -> ElephantHelperState { ElephantHelperState { location: "AA".to_string(), 
-    elephant_location: "AA".to_string(), open_valves: vec![], flow_rate: 0, 
-    last_move_was_to_open_current_valve: false,
-    // minutes_left: 30, 
-    total_released_pressure: 0 }}
+impl PartialOrd for SingleOperatorState {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self, other) {
+            (Self::Finished(a), Self::Finished(b)) => b.partial_cmp(a),            
+            (Self::Finished(_a), _) => Some(Ordering::Less),
+            (_, Self::Finished(_a)) => Some(Ordering::Greater),
+            _ => None,
+        }
+    }
+}
 
-    fn all_valves_are_open(&self, rooms: &HashMap<String,Room>) -> bool {
-        rooms.iter().all(|room| room.1.flow_rate == 0 || self.open_valves.contains(room.0))
+impl SingleOperatorState {
+    fn total_released_pressure(&self) -> usize {
+        match self {
+            Self::Finished(x) => *x,
+            Self::Ongoing(x) => x.total_released_pressure,
+        }
     }
 
-    fn successor_states(&self, rooms: &HashMap<String,Room>) -> Vec<ElephantHelperState> {
-        // if self.minutes_left == 0 { return vec![] };
-        // Outer product of self action and elephant action
-
-
-        let mut v1 = vec![];
-        let room = rooms.get(&self.location).unwrap();
-        if room.flow_rate > 0 && !self.open_valves.contains(&self.location) {
-            let mut valves = self.open_valves.clone();
-            valves.push(self.location.clone());
-            v1.push(ElephantHelperState { 
-                location: self.location.clone(),
-                elephant_location: self.elephant_location.clone(),
-                flow_rate: self.flow_rate + room.flow_rate,
-                // minutes_left: self.minutes_left - 1,
-                open_valves: valves,
-                last_move_was_to_open_current_valve: true,
-                total_released_pressure: self.total_released_pressure + self.flow_rate,  
-            })
-        }
-        for tunnel in room.tunnels_to_rooms.iter() {
-            v1.push(ElephantHelperState { 
-                location: tunnel.clone(), 
-                elephant_location: self.elephant_location.clone(),
-                flow_rate: self.flow_rate, 
-                open_valves: self.open_valves.clone(), 
-                last_move_was_to_open_current_valve: false,
-                // minutes_left: self.minutes_left - 1, 
-                total_released_pressure: self.total_released_pressure + self.flow_rate, 
-            })
-        }
-
-        let mut v2 = vec![];
-        let room = rooms.get(&self.elephant_location).unwrap();
-        if room.flow_rate > 0 && !self.open_valves.contains(&self.elephant_location) {
-            let mut valves = self.open_valves.clone();
-            valves.push(self.elephant_location.clone());
-            v2.push(ElephantHelperState { 
-                location: self.location.clone(),
-                elephant_location: self.elephant_location.clone(),
-                flow_rate: self.flow_rate + room.flow_rate,
-                // minutes_left: self.minutes_left - 1,
-                open_valves: valves,
-                last_move_was_to_open_current_valve: true,
-                total_released_pressure: self.total_released_pressure + self.flow_rate,  
-            })
-        }
-        for tunnel in room.tunnels_to_rooms.iter() {
-            v2.push(ElephantHelperState { 
-                location: self.location.clone(), 
-                elephant_location: tunnel.clone(),
-                flow_rate: self.flow_rate, 
-                open_valves: self.open_valves.clone(), 
-                last_move_was_to_open_current_valve: false,
-                // minutes_left: self.minutes_left - 1, 
-                total_released_pressure: self.total_released_pressure + self.flow_rate, 
-            })
-        }
-
-        // Merge. 
-        iproduct!(v1.into_iter(), v2.into_iter())
-            .filter_map(|(my_move,elephant_move)| {
-                if my_move.last_move_was_to_open_current_valve && elephant_move.last_move_was_to_open_current_valve {
-                    if my_move.location == elephant_move.elephant_location { 
-                        None
-                    } else {
-                        let mut valves = my_move.open_valves.clone();
-                        // println!("Current {:?}", self);
-                        // println!("{:?}, {:?}", valves, elephant_move.location);
-                        // assert!(!valves.contains(&self.elephant_location));
-                        valves.push(elephant_move.elephant_location.clone());            
-                        let flow_rate_in_elephant_room = rooms.get(&self.elephant_location).unwrap().flow_rate;                        
-
-                        Some(ElephantHelperState {
-                            location: my_move.location,
-                            elephant_location: elephant_move.elephant_location,
-                            flow_rate: my_move.flow_rate + flow_rate_in_elephant_room,
-                            open_valves: valves,
-                            last_move_was_to_open_current_valve: true,
-                            total_released_pressure: self.total_released_pressure + self.flow_rate,
-                        }) 
-                    }                     
+    fn successor_states(self, graph: &HashMap<LocationIndex, Room>) -> Vec<SingleOperatorState> {
+        match self {
+            Self::Finished(_x) => vec![self],
+            Self::Ongoing(state) => {
+                if state.all_valves_are_open() {
+                    vec![Self::Finished(state.total_released_pressure)]
                 } else {
-                    Some(ElephantHelperState {
-                        location: my_move.location,
-                        elephant_location: elephant_move.elephant_location,
-                        flow_rate: my_move.flow_rate.max(elephant_move.flow_rate),
-                        open_valves: if my_move.open_valves.len() > elephant_move.open_valves.len() { my_move.open_valves } else { elephant_move.open_valves },
-                        last_move_was_to_open_current_valve: false,
-                        total_released_pressure: self.total_released_pressure + self.flow_rate,
-                    })        
+                    graph[&state.location]
+                        .tunnels
+                        .iter()
+                        .filter(|t| !state.valve_open(&t.destination))
+                        .map(|t|                            
+                            if state.time_expended + (t.length as usize) + 1 >= 30 {
+                                Self::Finished(state.total_released_pressure)
+                            } else {
+                                let valve_turns_on_at = state.time_expended + (t.length as usize) + 1;
+                                Self::Ongoing( SingleOperatorStateInPlay { 
+                                    location: t.destination,
+                                    open_valves: state.open_valves + (1 << (t.destination as u64)),
+                                    time_expended: valve_turns_on_at,
+                                    total_released_pressure: state.total_released_pressure + graph[&t.destination].flow_rate * (30 - valve_turns_on_at)
+                                })
+                            })
+                        .collect()
                 }
-            })
-            .collect()
-    }
-
-    fn move_through(rooms: &HashMap<String,Room>) -> usize {
-
-        let mut states: Vec<ElephantHelperState> = vec![ElephantHelperState::new()];
-        for _minute in 1..=26 {
-            // Get all possible states
-            // Replace all states by their successor states.
-            states = states.into_iter()
-                .map(|state| state.successor_states(rooms).into_iter())
-                .flatten()
-                .sorted_by_key(|state| state.total_released_pressure)
-                .rev()
-                .take(100000)
-                .collect();
-    
-            // println!("Minute: {}, States: {:?}", minute, states);
+            }
         }
-        // 2772 too low
-    
-        let best = states.into_iter().max_by_key(|state| state.total_released_pressure).unwrap();
-        println!("Best state;: {:?}", best);
-        best.total_released_pressure
     }
+
+    fn move_through(start_from: &LocationIndex, graph: &HashMap<LocationIndex, Room>) -> usize {
+        let initial = {
+            let mut valves = u64::MAX; // All valves open.
+            // Close all valves with non-zero flow rate
+            for (index, room) in graph.iter() {
+                if room.flow_rate > 0 {
+                    valves ^= (1u64 << (*index as u64)) as u64;
+                }
+            }
+
+            SingleOperatorState::Ongoing(SingleOperatorStateInPlay { 
+                location: *start_from, 
+                open_valves: valves,
+                time_expended: 0,
+                total_released_pressure: 0 
+            })
+        };
+
+        let mut states: Vec<SingleOperatorState> = vec![initial];
+        // There will be as many iterations as there are states in the graph.
+        for _iteration in 0..graph.len() {
+            states = states.into_iter()
+                .map(|state| state.successor_states(graph) )
+                .flatten()
+                .sorted()
+                .dedup_by(|s1,s2| {
+                    match (s1, s2) {
+                        (SingleOperatorState::Finished(_a), SingleOperatorState::Finished(_b)) => true,
+                        _ => false,
+                    }
+                })
+                .collect();
+        }
+        states.iter().map(|s| s.total_released_pressure()).max().unwrap_or(0)
+
+    }
+
 }
 
+
+
+type LocationName = u16;
+
+fn name_to_usize(b: &[u8]) -> LocationName {
+    (b[0] as LocationName) * 256 + (b[1] as LocationName)
+}
+
+#[allow(dead_code)]
+fn usize_to_name(b: LocationName) -> String {
+    let b2: [u8;2] = [(b >> 8) as u8, (b & 255) as u8];
+    std::str::from_utf8(&b2[..]).unwrap().to_string()
+}
+
+#[derive(Debug,PartialEq, Eq)]
+struct RawInput {
+    name: LocationName,
+    flow_rate: usize,
+    tunnels: Vec<LocationName>,
+}
+
+const START: LocationName = (65 * 256) + 65; // AA
 
 pub fn solve(input: &str) -> Solution {
-    let rooms: HashMap<String,Room> = input.lines()
-        .map(|line| line.parse::<Room>().unwrap())
-        .map(|room| (room.name.clone(), room))
-        .collect();
+    let input: Vec<RawInput> = input.lines().map(|line| line.parse::<RawInput>().unwrap()).collect();
+    let location_indices: HashMap<LocationName,LocationIndex> = input.iter().enumerate().map(|x| (x.1.name, x.0 as LocationIndex)).collect();
 
-    let p1 = State::move_through(&rooms);
-    let p2 = ElephantHelperState::move_through(&rooms);
+    // Construct graph. For each room with a non-zero flow rate, or the initial room, calculate the shortest path to every other room.
+    let graph: HashMap<LocationIndex, Room> = (0..input.len()).filter_map(|i1| {
+        if input[i1].name == START || input[i1].flow_rate > 0 {
+            let v: Vec<Tunnel> = (0..input.len())
+                .filter(|i2| i1 != *i2 && input[*i2].flow_rate > 0)
+                .map(|i2| {
+                    let length: usize = 
+                        bfs(&i1, 
+                            |i| input[*i].tunnels.iter().map(|t| location_indices[t] as usize),
+                            |i| *i == i2).expect("No path!").len() - 1;
+                    Tunnel { destination: (i2 as LocationIndex), length: length as u8 }
+                })
+                .collect();
+            Some((i1 as LocationIndex, Room { flow_rate: input[i1].flow_rate, tunnels: v }))
+        } else {
+            None
+        }
+    }).collect();
 
-    Solution::new(p1,p2)
+    let p1 = SingleOperatorState::move_through(&location_indices[&START], &graph);
+
+    Solution::new(p1,0)
 }
 
 
+impl FromStr for RawInput {
+    type Err = &'static str;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let b = line.as_bytes();
+        let name = name_to_usize(&b[6..8]);
+        let flow_rate = line[23..].split(";").next().unwrap().parse::<usize>().unwrap();
+        let tunnels_to_rooms = (0..b.len()).step_by(4).map_while(|i| {
+            let start = b.len()-i-2;
+            let end = b.len()-i;
+            if b[start].is_ascii_uppercase() {
+                Some(name_to_usize(&b[start..end]))
+            } else {
+                None
+            }
+        }).collect();
+        Ok( RawInput { name, flow_rate, tunnels: tunnels_to_rooms })
+    }
+}
