1bcb92fe709c75efbffbbfdfe38e96302dbea6f3
Erik Aderstedt <erik@aderstedt.se>
2022-12-17 10:39:19 +0100

    Day 17 use u8 for each row in the grid

--- src/days_2022/day17.rs
+++ src/days_2022/day17.rs
@@ -10,40 +10,38 @@ enum Draft {
 }
 
 const NUM_P1_ITERATIONS: usize = 2022;
-const WIDTH: usize = 7;
+const WIDTH: u32 = 7;
 const MAX_HEIGHT: usize = 8000;
 const NUM_ITERATIONS: usize = 1000000000000;
 const NUM_FLOORS_TO_HASH: usize = 18;
 
-type Rock = [u8;16];
-type Grid = [u8;WIDTH*MAX_HEIGHT];
+type Rock = u32;
+type Grid = [u8;MAX_HEIGHT];
 
-const ROCK_A: Rock = [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0];
-const ROCK_B: Rock = [0,1,0,0,1,1,1,0,0,1,0,0,0,0,0,0];
-const ROCK_C: Rock = [1,1,1,0,0,0,1,0,0,0,1,0,0,0,0,0];
-const ROCK_D: Rock = [1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0];
-const ROCK_E: Rock = [1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0];
+const ROCK_A: Rock = 0b0000000_0000000_0000000_1111000;
+const ROCK_B: Rock = 0b0000000_0100000_1110000_0100000;
+const ROCK_C: Rock = 0b0000000_0010000_0010000_1110000;
+const ROCK_D: Rock = 0b1000000_1000000_1000000_1000000;
+const ROCK_E: Rock = 0b0000000_0000000_1100000_1100000;
 
 fn does_rock_fit(rock: &Rock, grid: &Grid, x0: usize, y0: usize) -> bool {
     // x0, y0 is the lower left corner of the rock.
-    (0..16).all(|i| {
-        let x = i % 4; let y = i / 4;
-        rock[i] == 0 || grid[(y0+y)*WIDTH + (x0 + x)] == 0
-    })
+    ((((rock >> WIDTH*0) & 0b1111000) >> x0) as u8) & grid[y0+0] == 0 &&
+    ((((rock >> WIDTH*1) & 0b1111000) >> x0) as u8) & grid[y0+1] == 0 &&
+    ((((rock >> WIDTH*2) & 0b1111000) >> x0) as u8) & grid[y0+2] == 0 &&
+    ((((rock >> WIDTH*3) & 0b1111000) >> x0) as u8) & grid[y0+3] == 0
 }
 
 fn paint_rock(rock: &Rock, grid: &mut Grid, x0: usize, y0: usize) {
     // x0, y0 is the lower left corner of the rock.
-    for i in 0..16 {
-        let x = i % 4; let y = i / 4;
-        if rock[i] == 1 {
-            grid[(y0+y)*WIDTH + (x0 + x)] = 1;
-        }
-    }
+    grid[y0+0] |= (((rock >> WIDTH*0) & 0b1111000) >> x0) as u8;
+    grid[y0+1] |= (((rock >> WIDTH*1) & 0b1111000) >> x0) as u8;
+    grid[y0+2] |= (((rock >> WIDTH*2) & 0b1111000) >> x0) as u8;
+    grid[y0+3] |= (((rock >> WIDTH*3) & 0b1111000) >> x0) as u8;
 }
 
 fn hash_top_18_rows(grid_part: &[u8]) -> u128 {
-    (0..(NUM_FLOORS_TO_HASH*WIDTH)).fold(0u128, |v,i| if grid_part[i] == 1 { v + (1u128 << i) } else { v })
+    (0..NUM_FLOORS_TO_HASH).fold(0u128, |v,i| v | (grid_part[i] as u128) << (8*i as u128))
 }
 
 #[derive(Hash,PartialEq, Eq)]
@@ -63,13 +61,13 @@ pub fn solve(input: &str) -> Solution {
 
     let rocks = [((4,1),ROCK_A), ((3,3),ROCK_B), ((3,3),ROCK_C), ((1,4),ROCK_D), ((2,2),ROCK_E)];
 
-    let mut grid: Grid = [0u8;WIDTH*MAX_HEIGHT];
+    let mut grid: Grid = [0u8;MAX_HEIGHT];
     let mut draft_source = drafts.iter().enumerate().cycle();
     let mut floor = 0;
     let mut seen_configurations: HashMap<Configuration,(usize,usize)> = HashMap::new();
     let mut p1: Option<usize> = None;
     let mut p2: Option<usize> = None;
-    for (_iteration, (rock_index,((width,height),rock))) in (0..NUM_ITERATIONS).zip(rocks.iter().enumerate().cycle()) {
+    for (_iteration, (rock_index,((width,height),rock))) in (1..=NUM_ITERATIONS).zip(rocks.iter().enumerate().cycle()) {
         let mut y = floor + 3;
         let mut x: i32 = 2;
         let draft_index = loop {
@@ -97,21 +95,21 @@ pub fn solve(input: &str) -> Solution {
             let configuration = Configuration {
                 rock: rock_index,
                 draft: draft_index,
-                grid: hash_top_18_rows(&grid[start*WIDTH .. (start + NUM_FLOORS_TO_HASH)*WIDTH])
+                grid: hash_top_18_rows(&grid[start..(start + NUM_FLOORS_TO_HASH)])
             };
             if let Some((iteration, floor_height)) = seen_configurations.get(&configuration) {
                 let iteration_period = _iteration - iteration;
                 if NUM_ITERATIONS % iteration_period == _iteration % iteration_period {
                     let floor_period = floor - floor_height;
                     let num_remaining = (NUM_ITERATIONS - _iteration) / iteration_period;
-                    p2 = Some(floor + num_remaining * floor_period - 1);
+                    p2 = Some(floor + num_remaining * floor_period);
                 } 
             } else {
                 seen_configurations.insert(configuration, (_iteration, floor));
             }
         }
 
-        if _iteration == NUM_P1_ITERATIONS - 1 {
+        if _iteration == NUM_P1_ITERATIONS {
             p1 = Some(floor);
         }
         if _iteration > NUM_P1_ITERATIONS && p2.is_some() {
