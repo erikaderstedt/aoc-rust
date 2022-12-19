6d115449de9cd6da817de8194aff868da0f15b13
Erik Aderstedt <erik@aderstedt.se>
2022-12-19 19:14:11 +0100

    Day 19 tune

--- src/days_2022/day19.rs
+++ src/days_2022/day19.rs
@@ -6,6 +6,7 @@ use std::str::FromStr;
 const NUM_MATERIALS: usize = 4;
 const NUM_INGREDIENTS: usize = 4;
 const FAR_FUTURE: u16 = 100;
+const GEODE: usize = 3;
 
 type Amount = u16;
 type RobotAmount = Amount;
@@ -18,77 +19,64 @@ struct Blueprint {
     best_geode_result: Amount
 }
 
-#[derive(Debug)]
-struct State {
-    materials: [Amount; NUM_MATERIALS],
-    robots: [RobotAmount; NUM_MATERIALS],
-    time_left: u16,
-}
-
 impl Blueprint {
 
-    fn simulate(&mut self, state: State) {
+    fn simulate(&mut self, materials: [Amount; NUM_MATERIALS], robots: [RobotAmount; NUM_MATERIALS], time_left: u16) {
         let mut robot_was_built = false;
 
         // At each iteration, we branch into building each available robot that has not yet reached the maximum
         // number required. The path that we trace out is the order in which we build the robots.
         for robot_type in 0..NUM_MATERIALS {
             // Do we build a robot of this type next?
-            if state.robots[robot_type] == self.max_robots[robot_type] {
+            if robots[robot_type] == self.max_robots[robot_type] {
+                // We don't need any more robots of this type.
                 continue;
             }
             let recipe = &self.costs[robot_type];
             let completion_time = (0..NUM_INGREDIENTS)
-                .filter_map(|material_type| match recipe[material_type] {
-                    0 => None, // This ore type is not required to build this robot type.
-                    x if x <= state.materials[material_type] => Some(0), // We already have enough for this robot.
-                    _ if state.robots[material_type] == 0 => Some(FAR_FUTURE), // There is no robot for this type of ore yet, so robot type 'i' is not available
-                    _ => Some((recipe[material_type] - state.materials[material_type] + state.robots[material_type] - 1) / state.robots[material_type]),
-                })
+                .map(|material_type| 
+                    if recipe[material_type] <= materials[material_type] {
+                        0 // We already have enough for this robot.
+                    } else if robots[material_type] == 0 {
+                        FAR_FUTURE // There is no robot for this type of ore yet, so robot type 'robot_type' is not available
+                    } else {
+                        (recipe[material_type] - materials[material_type] + robots[material_type] - 1) / robots[material_type]
+                    }
+                )
                 .max()
                 .unwrap() + 1;
 
-            if completion_time >= state.time_left {
+            if completion_time >= time_left {
                 continue;
             }
-            let time_remaining_when_finished = state.time_left - completion_time;
+            let time_remaining_when_finished = time_left - completion_time;
 
             let mut new_materials = [0; NUM_MATERIALS];
             let mut new_robots = [0; NUM_MATERIALS];
             for o in 0..NUM_MATERIALS {
-                new_materials[o] = state.materials[o] + state.robots[o] * completion_time - recipe[o];
-                new_robots[o] = state.robots[o] + u16::from(o == robot_type);
+                new_materials[o] = materials[o] + robots[o] * completion_time - recipe[o];
+                new_robots[o] = robots[o] + u16::from(o == robot_type);
             }
 
             // If we were to build only geode robots every turn after building the robot, could we beat the current max?
-            if ((time_remaining_when_finished - 1) * time_remaining_when_finished) / 2
-                + new_materials[3] + time_remaining_when_finished * new_robots[3]
-                < self.best_geode_result {
+            // (and, if the other three types of robots are maxed out, this is the case)
+            if ((time_remaining_when_finished - 1) * time_remaining_when_finished) / 2  // Aritmetic series series
+                + new_materials[GEODE]  // What we have now
+                + time_remaining_when_finished * new_robots[GEODE] // What the current number of robots are producing
+                <= self.best_geode_result {
                 continue;
             }
             robot_was_built = true;
-            self.simulate(
-                State {
-                    materials: new_materials,
-                    robots: new_robots,
-                    time_left: time_remaining_when_finished,
-                }
-            );
+            self.simulate(new_materials, new_robots, time_remaining_when_finished);
         }
         if !robot_was_built {
             // We couldn't make new robots. Calculate the number of geodes we end up with if we let the clock run out
-            self.best_geode_result = self.best_geode_result.max(state.materials[3] + state.robots[3] * state.time_left as u16);
+            self.best_geode_result = self.best_geode_result.max(materials[GEODE] + robots[GEODE] * time_left as u16);
         }
     }
 
     fn run_simulation(&mut self, max_time: u16) -> Amount {
-        self.simulate(
-            State {
-                materials: [0; NUM_MATERIALS],
-                robots: [1, 0, 0, 0],
-                time_left: max_time
-            },
-        );
+        self.simulate([0; NUM_MATERIALS], [1, 0, 0, 0], max_time);
         self.best_geode_result
     }
 }
