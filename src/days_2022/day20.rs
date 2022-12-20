297e9db79a5c3db74bfbe4e705834e52571bd058
Erik Aderstedt <erik.aderstedt@hasselblad.com>
2022-12-20 09:48:13 +0100

    Day 20 cleanup

--- src/days_2022/day20.rs
+++ src/days_2022/day20.rs
@@ -1,36 +1,28 @@
 // https://adventofcode.com/2022/day/20
 
 use itertools::Itertools;
-
 use crate::common::Solution;
 use std::collections::VecDeque;
 
 type List = VecDeque<(usize,i64)>;
 
-struct ListMixer {
-    ordering: List,
-    mixed: List
-}
+fn mix(ordering: List, num_iterations: usize) -> i64 {
+    let mut mixed = ordering.clone();
+    let len = mixed.len() - 1;
 
-impl ListMixer {
-    fn new(list: List) -> ListMixer { ListMixer { ordering: list.clone(), mixed: list }}
-    
-    fn mix(&mut self) {    
-        for (label, num) in self.ordering.iter() {
-            let index: usize = self.mixed.iter().find_position(|j| j.0 == *label).unwrap().0;
-            let value = self.mixed.remove(index).unwrap();
-            let new_index = (((index as i64) + *num + (2811589153)*(self.mixed.len() as i64)) as usize) % self.mixed.len();
-            self.mixed.insert(new_index, value);
-        }
+    for _i in 0..num_iterations {
+        for (label, num) in ordering.iter() {
+            let index: usize = mixed.iter().find_position(|j| j.0 == *label).unwrap().0;
+            let value = mixed.remove(index).unwrap();
+            let new_index = (((index as i64) + *num + (2811589153)*(len as i64)) as usize) % len;
+            mixed.insert(new_index, value);
+        }    
     }
 
-    fn grove_coordinates(&self) -> i64 {
-        let i0 = self.mixed.iter().find_position(|&j| j.1 == 0i64).unwrap().0;
-        let n1 = self.mixed[(i0 + 1000) % self.mixed.len()].1;
-        let n2 =  self.mixed[(i0 + 2000) % self.mixed.len()].1;
-        let n3 =  self.mixed[(i0 + 3000) % self.mixed.len()].1;
-        n1+n2+n3
-    }
+    let i0 = mixed.iter().find_position(|&j| j.1 == 0i64).unwrap().0;
+    [1000,2000,3000].iter()
+        .map(|j| mixed[(i0 + *j) % mixed.len()].1)
+        .sum()
 }
 
 pub fn solve(input: &str) -> Solution {
@@ -44,19 +36,8 @@ pub fn solve(input: &str) -> Solution {
         .map(|(i, n)| (i.clone(), n * 811589153))
         .collect();
 
-    let p1 = {
-        let mut mixer = ListMixer::new(list);
-        mixer.mix();
-        mixer.grove_coordinates()
-    };
-
-    let p2 = {
-        let mut mixer = ListMixer::new(decrypted);
-        for _ in 0..10 {
-            mixer.mix();            
-        }
-        mixer.grove_coordinates()
-    };
+    let p1 = mix(list, 1);
+    let p2 = mix(decrypted, 10);
 
     Solution::new(p1,p2)
 }
