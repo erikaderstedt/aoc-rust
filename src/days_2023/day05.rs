a15fb479f2174e76961ff4c705737e38221b6549
Erik Aderstedt <erik@aderstedt.se>
2023-12-05 10:39:01 +0100

    Day 5 optimized

--- src/days_2023/day05.rs
+++ src/days_2023/day05.rs
@@ -3,77 +3,72 @@
 use itertools::Itertools;
 use range_ext::intersect::{Intersect, IntersectionExt};
 use crate::common::Solution;
+use std::ops::Range;
 
 pub fn solve(input: &str) -> Solution {    
+    let mut seeds: Vec<u64> = input.lines().next().unwrap().split(": ").last().unwrap().split(' ').map(|x| x.parse::<u64>().unwrap()).collect();
+    let mut seed_ranges: Vec<Range<u64>> = (0..(seeds.len() >> 1)).map(|i| seeds[2*i]..(seeds[2*i] + seeds[2*i+1])).collect();
 
-    let mut seeds: Vec<usize> = input.lines().next().unwrap().split(": ").last().unwrap().split(' ').map(|x| x.parse::<usize>().unwrap()).collect();
-    let mut seed_ranges: Vec<std::ops::Range<usize>> = (0..(seeds.len() >> 1)).map(|i| seeds[2*i]..(seeds[2*i] + seeds[2*i+1])).collect();
+    let maps: Vec<Vec<(u64,Range<u64>)>> = 
+        input.split("\n\n").skip(1)
+            .map(|p| 
+                p.lines().skip(1).map(|line| {
+                    let (dest_start, map_start, length) = line.split(' ').map(|x| x.parse::<u64>().unwrap()).collect_tuple().unwrap();
+                    (dest_start, map_start..(map_start+length))
+            })
+            .collect()
+        ).collect();
     
-    for map in input.split("\n\n").skip(1) {
-        let mut moved = vec![false; seeds.len()];
-
-        for r in map.lines().skip(1) {
-            let (dest_start, map_start, length) = r.split(' ').map(|x| x.parse::<usize>().unwrap()).collect_tuple().unwrap();
-            
-            for (i,s) in seeds.iter_mut().enumerate() {
-                if moved[i] { 
-                    continue;
-                }
-                if (map_start..(map_start+length)).contains(s) {
-                    *s = *s - map_start + dest_start;
-                    moved[i] = true;
+    for map in maps.iter() {
+        for s in seeds.iter_mut() {
+            for (destination, source_range) in map.iter() {
+                if source_range.contains(s) {
+                    *s = *s + destination - source_range.start;
+                    break;
                 }
             }
         }
     }
     let p1 = seeds.iter().min().unwrap();
     
-    for map in input.split("\n\n").skip(1) {
-        let mut moved = vec![false; seed_ranges.len()];
-        let mut new_ranges = Vec::new();
-
-        for r in map.lines().skip(1) {
-            let (dest_start, map_start, length) = r.split(' ').map(|x| x.parse::<usize>().unwrap()).collect_tuple().unwrap();
-            let m = map_start..(map_start+length);
-
-            let mut new_unmoved_ranges = Vec::new();
-            for (i,s) in seed_ranges.iter_mut().enumerate() {
-                if moved[i] { 
-                    continue;
-                }
-                match s.intersect_ext(&m) {
-                    IntersectionExt::Empty | IntersectionExt::Less | IntersectionExt::Greater => {},
-                    IntersectionExt::Within | IntersectionExt::Same => {
-                        s.start = s.start + dest_start - map_start;
-                        s.end = s.end + dest_start - map_start;
-                        moved[i] = true;
-                    },
-                    IntersectionExt::LessOverlap => {
-                        new_ranges.push(dest_start..(dest_start + s.end - m.start));
-                        s.end = m.start;
-                    },
-                    IntersectionExt::GreaterOverlap => {
-                        let length = m.end - s.start;
-                        let start = s.start + dest_start - m.start;
-                        new_ranges.push(start..(start+length));
-                        s.start = m.end;
-                    },
-                    IntersectionExt::Over => {
-                        new_ranges.push(dest_start..(dest_start+length));
-                        new_unmoved_ranges.push(m.end..s.end);                    
-                        s.end = m.start;
-                    },                    
-                };
-            }
-
-            moved.extend(new_unmoved_ranges.iter().map(|_| false));
-            seed_ranges.extend(new_unmoved_ranges.into_iter());
-
-        }
-        seed_ranges.extend(new_ranges.into_iter());
-
+    for map in maps.iter() {
+        seed_ranges = seed_ranges.into_iter().map(|seed| mapped_range(seed, map)).flatten().collect();
     }
-    let p2 = seed_ranges.iter().map(|r| r.start).min().unwrap();
+    let p2 = seed_ranges.iter().min_by_key(|r| r.start).unwrap().start;
 
     Solution::new(p1,p2)
+}
+
+fn mapped_range(seed: Range<u64>, source_ranges: &Vec<(u64,Range<u64>)>) -> Vec<Range<u64>> {
+    let mut new_ranges = Vec::new();
+    let mut seed = seed.clone();
+    let mut add_remainder_to_output = true;
+
+    for (destination, range) in source_ranges.iter() {
+        match seed.intersect_ext(range) {
+            IntersectionExt::Empty | IntersectionExt::Less | IntersectionExt::Greater => {},
+            IntersectionExt::Within | IntersectionExt::Same => {
+                new_ranges.push((seed.start + *destination - range.start)..(seed.end + *destination - range.start));
+                add_remainder_to_output = false;
+                break;
+            },
+            IntersectionExt::LessOverlap => {
+                new_ranges.push(*destination..(*destination + seed.end - range.start));
+                seed.end = range.start;
+            },
+            IntersectionExt::GreaterOverlap => {
+                new_ranges.push((seed.start + *destination - range.start)..(*destination + range.end - range.start));
+                seed.start = range.end;
+            },
+            IntersectionExt::Over => {
+                new_ranges.push(*destination..(*destination+(range.end - range.start)));
+                new_ranges.extend(mapped_range(range.end..seed.end, source_ranges));
+                seed.end = range.start;
+            },
+        }
+    }
+    if add_remainder_to_output {
+        new_ranges.push(seed);
+    }
+    new_ranges
 }
