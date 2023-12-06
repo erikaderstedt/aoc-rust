17d0c24296247b645524c5231969ca4137c3991f
Erik Aderstedt <erik@aderstedt.se>
2023-12-06 07:34:11 +0100

    Cleanup day 6

--- src/days_2023/day06.rs
+++ src/days_2023/day06.rs
@@ -1,36 +1,31 @@
-// https://adventofcode.com/2023/day/5
+// https://adventofcode.com/2023/day/6
 
 use crate::common::Solution;
+const SURROUNDING: usize = 2;
 
-fn number_of_wins(time_and_distance : (&usize, &usize)) -> usize {
-    let time = time_and_distance.0.clone();
-    let distance = time_and_distance.1.clone();
+fn number_of_wins(time: usize, distance: usize) -> usize {
     let approximate_start = (time - (((time*time - 4*distance) as f64).sqrt() as usize)) / 2;
     let approximate_end = (time + (((time*time - 4*distance) as f64).sqrt() as usize)) / 2;
+    let around = |s: usize| -> std::ops::Range<usize> {
+        s.checked_sub(SURROUNDING).unwrap_or(0)..(s + SURROUNDING + 1)
+    };
 
-    let start = (approximate_start.checked_sub(2).unwrap_or(0)..(approximate_start + 2))
-        .find(|t| t * (time - t) > distance).unwrap();
-    let end = (approximate_end.checked_sub(2).unwrap_or(0)..(approximate_end + 2)).rev()
-        .find(|t| t * (time - t) > distance).unwrap();
+    let start = around(approximate_start).find(|t| t * (time - t) > distance).unwrap();
+    let end = around(approximate_end).rev().find(|t| t * (time - t) > distance).unwrap();
     
     end - start + 1
 }
 
 pub fn solve(_input: &str) -> Solution {
-
-    // let times: Vec<usize> = input.lines().next().unwrap().split(":").last().unwrap()
-    //                             .split(" ").filter_map(|x| x.parse::<usize>().ok()).collect();
-    // let distances: Vec<usize> = input.lines().skip(1).next().unwrap().split(":").last().unwrap()
-    //                         .split(" ").filter_map(|x| x.parse::<usize>().ok()).collect();
     let times = vec![47,70,75,66];
     let distances = vec![282,1079,1147,1062];
-    let p1: usize = times.iter().zip(distances.iter()).map(number_of_wins).product();
+    let p1: usize = times.into_iter()
+        .zip(distances.into_iter())
+        .map(|(t, d)| number_of_wins(t, d)).product();
 
-    // let time: usize = input.lines().next().unwrap().replace(" ", "").split(":").last().unwrap().parse::<usize>().unwrap();
-    // let distance: usize = input.lines().skip(1).next().unwrap().replace(" ", "").split(":").last().unwrap().parse::<usize>().unwrap();
     let time: usize = 47707566;
     let distance: usize = 282107911471062;
-    let p2 = number_of_wins((&time, &distance));
+    let p2 = number_of_wins(time, distance);
 
     Solution::new(p1,p2)
 }
