4be68835879fb7d3b20051ae248752a48a3ae43e
Erik Aderstedt <erik@aderstedt.se>
2023-12-15 07:32:53 +0100

    Day 15 optimized

--- src/days_2023/day15.rs
+++ src/days_2023/day15.rs
@@ -2,41 +2,52 @@
 
 use crate::common::Solution;
 
+enum Operation {
+    InsertOrReplace(usize),
+    Remove
+}
+
+const EMPTY : Vec<(&str, usize)> = Vec::new();
+
 pub fn solve(input: &str) -> Solution {
 
-    let p1: u64 = input.split(',').map(|step| {
-        step.as_bytes().iter().fold(0u64, |current_value, b| {
-            ((current_value + *b as u64) * 17) & 0xff
-        })
-    }).sum();
+    let mut p1 = 0;
+    let mut boxes: [Vec<(&str, usize)>;256] = [EMPTY; 256];
 
-    let mut boxes: Vec<Vec<(&str, usize)>> = (0..256).map(|_| Vec::new()).collect();
+    for operation_description in input.split(',') {
+        let len = operation_description.len();
+        let (label, operation) = if operation_description.contains('-') {
+            (&operation_description[0..len-1], Operation::Remove)
+        } else if operation_description.contains('=') {
+            (&operation_description[0..len-2], Operation::InsertOrReplace(operation_description[len-1..len].parse::<usize>().unwrap()))
+        } else {
+            panic!("Invalid operation")
+        };
 
-    for operation in input.split(',') {
-        let label: String = operation.chars().take_while(|c| c.is_alphabetic()).collect();
         let id: usize = label.as_bytes().iter().fold(0u64, |current_value, b| {
             ((current_value + *b as u64) * 17) & 0xff
         }) as usize;
 
-        if operation.contains('-') {
-            if let Some(i) = boxes[id].iter().position(|l| l.0 == label) {
-                boxes[id].remove(i);
+        p1 += id;
+
+        match operation {
+            Operation::Remove => {
+                if let Some(i) = boxes[id].iter().position(|l| l.0 == label) {
+                    boxes[id].remove(i);
+                }
+            },
+            Operation::InsertOrReplace(focal_length) => {
+                if let Some(i) = boxes[id].iter().position(|l| l.0 == label) {
+                    boxes[id][i].1 = focal_length;
+                } else {
+                    boxes[id].push((label, focal_length))
+                }
             }
-        } else if operation.contains('=') {
-            let (l2, n) = operation.split_once('=').unwrap();
-            let nn = n.parse::<usize>().unwrap();
-            if let Some(i) = boxes[id].iter().position(|l| l.0 == label) {
-                boxes[id][i].1 = nn;
-            } else {
-                boxes[id].push((&l2, nn))
-            }
-        }        
+        }
     }
 
-    let p2 = boxes.into_iter().enumerate().fold(0, |total, (index,b)| {
-        total + b.iter().enumerate().map(|(slot, lens)| {
-            (index + 1) * (slot + 1) * lens.1
-        }).sum::<usize>()
+    let p2 = boxes.iter().enumerate().fold(0, |total, (index,b)| {
+        total + (index + 1) * b.iter().enumerate().map(|(slot, lens)| (slot + 1) * lens.1 ).sum::<usize>()
     });
     
     Solution::new(p1, p2)
