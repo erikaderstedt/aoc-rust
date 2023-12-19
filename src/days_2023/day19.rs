04dedf2f5e253392378478db7d0d94b853f5cc8c
Erik Aderstedt <erik@aderstedt.se>
2023-12-19 10:00:59 +0100

    Day 19 optimized some more

--- src/days_2023/day19.rs
+++ src/days_2023/day19.rs
@@ -12,8 +12,8 @@ enum Destination<'a> {
 
 enum Condition {
     Always,
-    LessThan(u8,usize),
-    GreaterThan(u8,usize),
+    LessThan(usize,usize),
+    GreaterThan(usize,usize),
 }
 
 struct Rule<'a> {
@@ -30,15 +30,12 @@ enum Limit {
 }
 
 struct Search {
-    x: std::ops::RangeInclusive<usize>,
-    m: std::ops::RangeInclusive<usize>,
-    a: std::ops::RangeInclusive<usize>,
-    s: std::ops::RangeInclusive<usize>,
+    v: [std::ops::RangeInclusive<usize>; 4],
 }
 
 impl Search {
     fn new() -> Search {
-        Search { x: 1..=4000, m: 1..=4000, a: 1..=4000, s: 1..=4000 }
+        Search { v: [1..=4000, 1..=4000, 1..=4000, 1..=4000] }
     }
 
     fn impose_limit(range: &std::ops::RangeInclusive<usize>, limit: Limit) -> std::ops::RangeInclusive<usize> {
@@ -50,21 +47,15 @@ impl Search {
         }
     } 
 
-    fn inserting(&self, u: u8, limit: Limit) -> Self {
-        match u {
-            b'x' => Search { x: Search::impose_limit(&self.x, limit), m: self.m.clone(), a: self.a.clone(), s: self.s.clone() },
-            b'm' => Search { x: self.x.clone(), m: Search::impose_limit(&self.m, limit), a: self.a.clone(), s: self.s.clone() },
-            b'a' => Search { x: self.x.clone(), m: self.m.clone(), a: Search::impose_limit(&self.a, limit), s: self.s.clone() },
-            b's' => Search { x: self.x.clone(), m: self.m.clone(), a: self.a.clone(), s: Search::impose_limit(&self.s, limit) },
-            _ => unreachable!()
-        }
+    fn inserting(&self, u: usize, limit: Limit) -> Self {
+        let mut v = self.v.clone();
+        v[u] = Search::impose_limit(&v[u], limit);
+        Search { v }
     }
 
     fn total(&self) -> usize {
-        (if self.x.end() >= self.x.start() { self.x.end() - self.x.start() + 1} else { 0 }) * 
-        (if self.m.end() >= self.m.start() { self.m.end() - self.m.start() + 1} else { 0 }) * 
-        (if self.a.end() >= self.a.start() { self.a.end() - self.a.start() + 1} else { 0 }) * 
-        (if self.s.end() >= self.s.start() { self.s.end() - self.s.start() + 1} else { 0 })
+        // In our input there is no case where the conditions go to zero, so these if's are not really needed.
+        self.v.iter().map(|r| if r.end() >= r.start() { r.end() - r.start() + 1 } else { 0 }).product()
     }
 
     fn apply(&self, current_workflow: &Vec<Rule>, index: usize, workflows: &HashMap<&str, Vec<Rule>>) -> usize {
@@ -104,17 +95,17 @@ impl Search {
 pub fn solve(input: &str) -> Solution {
     let (s1, s2) = input.split_once("\n\n").unwrap();
     
-    let workflows: HashMap<&str, Vec<Rule>> = s1.lines()
-    .map(|line| -> (&str, Vec<Rule>) {
-        let (name, rules) = line.split_once('{').unwrap();        
-        let (rules, _) = rules.split_once('}').unwrap();
+    let workflows: HashMap<&str, Vec<Rule>> = s1.lines().map(|line| -> (&str, Vec<Rule>) {
+        let (name, rules) = line.split_once('{').unwrap();
         
-        let rules: Vec<Rule> = rules.split(',').map(|s|
+        let rules: Vec<Rule> = rules[..rules.len()-1].split(',').map(|s|
             match s.as_bytes()[0] {
                 b'A' => Rule { condition: Condition::Always, destination: Destination::Accepted },
                 b'R' => Rule { condition: Condition::Always, destination: Destination::Rejected },
                 b'x' | b'm' | b'a' | b's' => {
-                    let part = s.as_bytes()[0].clone(); 
+                    let part = match s.as_bytes()[0] {
+                        b'x' => 0, b'm' => 1, b'a' => 2, b's' => 3, _ => unreachable!(),
+                    };
                     let condition = match s.as_bytes()[1] {
                         b'<' => { 
                             let q: String = s.chars().filter(|c| c.is_ascii_digit()).collect();
@@ -141,13 +132,12 @@ pub fn solve(input: &str) -> Solution {
                 },
                 _ => Rule { condition: Condition::Always, destination: Destination::Workflow(s) },
             }).collect();
-
         (name, rules)
     }).collect();
     
     let p1: usize = s2.lines().map(|line| -> usize {
-        let h:HashMap<u8,usize> = line[1..line.len()-1].split(',')
-            .map(|s| (s.as_bytes()[0].clone(), s[2..].parse::<usize>().unwrap())).collect();
+        let h:Vec<usize> = line[1..line.len()-1].split(',')
+            .map(|s| s[2..].parse::<usize>().unwrap()).collect();
 
         let mut workflow = "in";
         loop {
@@ -155,12 +145,12 @@ pub fn solve(input: &str) -> Solution {
             let destination = w.iter().find_map(|rule| 
                 match rule.condition {
                     Condition::Always => { Some(rule.destination)},
-                    Condition::GreaterThan(p,value) if h[&p] > value => { Some(rule.destination) },
-                    Condition::LessThan(p,value) if h[&p] < value => { Some(rule.destination) },
+                    Condition::GreaterThan(p,value) if h[p] > value => { Some(rule.destination) },
+                    Condition::LessThan(p,value) if h[p] < value => { Some(rule.destination) },
                     _ => None,
                 }).unwrap();
             match destination {
-                Destination::Accepted => break h.values().sum(),
+                Destination::Accepted => break h.iter().sum(),
                 Destination::Rejected => break 0,
                 Destination::Workflow(x) => workflow = x,
             };
