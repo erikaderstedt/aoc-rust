b8f07b3c2a325e04ce81a701adca3a41be945121
Erik Aderstedt <erik@aderstedt.se>
2023-12-20 20:09:49 +0100

    2023 day 20 analyze loops

--- src/days_2023/day20.rs
+++ src/days_2023/day20.rs
@@ -6,15 +6,40 @@ use crate::common::Solution;
 
 const P1_BUTTON_PRESSES: usize = 1000;
 const APPROX_SIGNALS_SENT_PER_BUTTON_PRESS: usize = 200;
+const RX_INDEX: usize = usize::MAX;
+const NUM_BITS: usize = 12;
 
-#[derive(PartialEq, Eq,Clone, Debug,Copy)]
+#[derive(PartialEq, Eq)]
 enum Kind {
     FlipFlop(bool),
     Conjunction(u64,u32),
     Broadcaster,
 }
 
-#[derive(Debug)]
+impl Kind {
+    fn tranceive(&mut self, signal: bool, source: usize) -> Option<bool> {
+        match self {
+            Kind::Broadcaster => Some(signal),
+            Kind::FlipFlop(state) => {
+                if signal == false {
+                    *state = !*state;
+                    Some(*state)
+                } else {
+                    None
+                }},
+            Kind::Conjunction(inputs, num_inputs) => {
+                if signal {
+                    *inputs |= 1 << source;
+                } else {
+                    *inputs &= u64::MAX ^ (1 << source);
+                }
+                let all_inputs_high = inputs.count_ones() == *num_inputs;
+                Some(!all_inputs_high)
+            }
+        }
+    }
+}
+
 struct Module {
     kind: Kind,
     destination_modules: Vec<usize>,
@@ -31,7 +56,7 @@ pub fn solve(input: &str) -> Solution {
             }, index)})
         .collect();
 
-    names.insert("rx", usize::MAX);
+    names.insert("rx", RX_INDEX);
 
     let mut modules: Vec<Module> = input.lines()
         .map(|line| {
@@ -57,76 +82,54 @@ pub fn solve(input: &str) -> Solution {
             }})
         .collect();
 
-    let outputs_to_rx = modules.iter().position(|m| m.destination_modules[0] == usize::MAX).unwrap();
-    let outputs_to_that_node: usize = modules
-        .iter()
-        .filter(|module| module.destination_modules.contains(&outputs_to_rx))
-        .count();
-    let mut deltas: Vec<usize> = vec![];
     let mut num_low = 0;
     let mut num_high = 0;
-    let mut button_press = 0;
+    let broadcaster = names["broadcaster"];
 
-    loop {
+    for _button_press in 0..P1_BUTTON_PRESSES {
         let mut v: Vec<(bool, usize, usize)> = Vec::with_capacity(APPROX_SIGNALS_SENT_PER_BUTTON_PRESS);
         let mut v_index = 0;
-        v.push((false, names["broadcaster"], 0));
-        button_press += 1;
-        if button_press <= P1_BUTTON_PRESSES { num_low += 1; }
+        v.push((false, broadcaster, 0));
+        num_low += 1;
 
         while v_index < v.len() {
             let (signal, module_index, source_index) = v[v_index];
             v_index += 1;
-
-            if signal == true && module_index == outputs_to_rx {
-                // Depends on one cycle not being more than 2x other cycles.
-                deltas.push(button_press);
-            }
             match modules.get_mut(module_index) {
                 Some(module) => {
-                    let pulse_to_send: Option<bool> = match module.kind {
-                        Kind::Broadcaster => Some(signal),
-                        Kind::FlipFlop(mut state) => {
-                            if signal == false {
-                                state = !state;
-                                module.kind = Kind::FlipFlop(state);
-                                Some(state)
-                            } else {
-                                None
-                            }},
-                        Kind::Conjunction(mut inputs, num_inputs) => {
-                            if signal {
-                                inputs |= 1 << source_index;
-                            } else {
-                                inputs &= u64::MAX ^ (1 << source_index);
-                            }
-                            let all_inputs_high = inputs.count_ones() == num_inputs;
-                            module.kind = Kind::Conjunction(inputs, num_inputs);
-                            Some(!all_inputs_high)
-                        }
-                    };
-                    if let Some(pulse_to_send) = pulse_to_send {
-                        if button_press <= P1_BUTTON_PRESSES {
+                    if let Some(pulse_to_send) = module.kind.tranceive(signal, source_index) {
                             if pulse_to_send { 
                                 num_high += module.destination_modules.len() 
                             } else { 
                                 num_low += module.destination_modules.len()
                             }
-                        }
                         v.extend(module.destination_modules.iter().map(|n| (pulse_to_send, *n, module_index) ));
                     }
                 },
                 None => { },
             }
         }
-
-        if deltas.len() == outputs_to_that_node && button_press > P1_BUTTON_PRESSES {
-            break;
-        }
     }
 
     let p1 = num_high * num_low;
-    let p2: usize = deltas.into_iter().take(outputs_to_that_node).product();
+
+    // Each broadcaster output leads to a counter loop. This is a stricter assumption than the
+    // previous implementation, but I still feel it is ok.
+    let p2: usize = modules[broadcaster].destination_modules.iter()
+        .map(|m| {
+            // Two output nodes. One is the counter, and the other is the second bit.
+            let counter = *(modules[*m].destination_modules.iter().find(|x| modules[**x].destination_modules.len() > 2).unwrap());
+            let all_bits_except_the_last = (0..(NUM_BITS-1))
+                .fold((0, m), |(mut value, module_index), shift| {
+                    let next = modules[*module_index].destination_modules.iter().find(|x| **x != counter).unwrap();
+                    if modules[*module_index].destination_modules.len() > 1 {
+                        value += 1 << shift;
+                    };
+                    (value, next)
+                }).0;
+            all_bits_except_the_last + (1 << (NUM_BITS-1)) // Last bit is always set. As is the first one.
+        })
+        .product();
 
     Solution::new(p1, p2)
 }
