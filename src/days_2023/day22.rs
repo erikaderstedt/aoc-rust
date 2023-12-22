cc17b04cd4457dcb5feac92f28caac867a5d281f
Erik Aderstedt <erik@aderstedt.se>
2023-12-22 08:29:01 +0100

    Day 22 optimized

--- src/days_2023/day22.rs
+++ src/days_2023/day22.rs
@@ -1,30 +1,41 @@
 // https://adventofcode.com/2023/day/22
 
 use std::{str::FromStr, collections::HashSet};
-
 use itertools::Itertools;
-
 use crate::common::{Solution, parsed_from_each_line};
 
 #[derive(Clone)]
 struct Brick {
-    start: (usize, usize, usize),
-    end: (usize, usize, usize),
+    start: (u8, u8, u16),
+    end: (u8, u8, u16),
 }
+
+type Space = Vec<Vec<Vec<u16>>>;
 
 impl Brick {
     
-    fn initial(start: (usize, usize, usize), end: (usize, usize, usize)) -> Brick {
-        Brick { start, end }
+    fn initial(start: (u16, u16, u16), end: (u16, u16, u16)) -> Brick {
+        Brick { start: (start.0 as u8, start.1 as u8, start.2), 
+            end: (end.0 as u8, end.1 as u8, end.2) }
     }
 
-    fn above(&self, space: &Vec<Vec<Vec<usize>>>) -> HashSet<usize> {
-        let mut above: HashSet<usize> = HashSet::new();
-        let z = self.end.2;
+    fn configure_space(&self, space: &mut Space, index: u16) {
+        for x in self.start.0..=self.end.0 {
+            for y in self.start.1..=self.end.1 {
+                for z in self.start.2..=self.end.2 {
+                    space[x as usize][y as usize][z as usize] = index;
+                }
+            }
+        }
+    }
+
+    fn above(&self, space: &Space) -> HashSet<u16> {
+        let mut above: HashSet<u16> = HashSet::new();
+        let z = self.end.2 as usize;
 
         for x in self.start.0..=self.end.0 {
             for y in self.start.1..=self.end.1 {
-                let v = space[x][y][z+1];
+                let v = space[x as usize][y as usize][z+1];
                 if v != 0 {
                     above.insert(v);
                 }
@@ -34,13 +45,13 @@ impl Brick {
         above
     }
 
-    fn below(&self, space: &Vec<Vec<Vec<usize>>>) -> HashSet<usize> {
-        let mut below: HashSet<usize> = HashSet::new();
-        let z = self.start.2;
+    fn below(&self, space: &Space) -> HashSet<u16> {
+        let mut below: HashSet<u16> = HashSet::new();
+        let z = self.start.2 as usize;
 
         for x in self.start.0..=self.end.0 {
             for y in self.start.1..=self.end.1 {
-                let v = space[x][y][z-1];
+                let v = space[x as usize][y as usize][z-1];
                 if v != 0 {
                     below.insert(v);
                 }
@@ -51,68 +62,51 @@ impl Brick {
     }
 }
 
-fn drop_bricks(bricks: &mut Vec<Brick>, space: &mut Vec<Vec<Vec<usize>>>) -> usize {
-    let mut total_moved: HashSet<usize> = HashSet::new();
+fn drop_bricks(bricks: &mut Vec<Brick>, space: &mut Space) -> usize {
+
+    let mut total_moved = vec![false; bricks.len()];
+    let mut first_moved: Option<usize> = None;
     loop {
-        let mut moved = 0;
-        for (index,brick) in bricks.iter_mut().enumerate() {
-            let z = brick.start.2.min(brick.end.2);
-            if z == 1 { continue; }
+        let mut num_moved = 0;
+        for (index,brick) in bricks.iter_mut().enumerate().skip(first_moved.unwrap_or(0)) {
+            let z = brick.start.2 as usize;
 
             if let Some(new_z) = 
                 (1..z).rev()
                     .take_while(|z|
                         (brick.start.0..=brick.end.0)
                             .all(|x| (brick.start.1..=brick.end.1)
-                                .all(|y| space[x][y][*z] == 0)))
+                                .all(|y| space[x as usize][y as usize][*z] == 0)))
                     .last() {
                         
-                    for x in brick.start.0..=brick.end.0 {
-                        for y in brick.start.1..=brick.end.1 {
-                            for z in brick.start.2..=brick.end.2 {
-                                space[x][y][z] = 0;
-                            }
-                        }
+                    brick.configure_space(space, 0);
+
+                    brick.end.2 = brick.end.2 - (z - new_z) as u16;
+                    brick.start.2 = new_z as u16;
+
+                    brick.configure_space(space, (index as u16) +1);
+
+                    if first_moved.is_none() {
+                        first_moved = Some(index);
                     }
-
-                    brick.end.2 -= z - new_z;
-                    brick.start.2 = new_z;
-
-                    for x in brick.start.0..=brick.end.0 {
-                        for y in brick.start.1..=brick.end.1 {
-                            for z in brick.start.2..=brick.end.2 {
-                                space[x][y][z] = index + 1;
-                            }
-                        }
-                    }
-                    total_moved.insert(index);
-                    moved += 1;
-
+                    total_moved[index] = true;
+                    num_moved += 1;
             }
         }
-        if moved == 0 { break; }
+        if num_moved == 0 { break; }
     }
-    total_moved.len()
+    total_moved.into_iter().filter(|t| *t).count()
 }
 
 pub fn solve(input: &str) -> Solution {
     let mut bricks: Vec<Brick> = parsed_from_each_line(input);
+    let mut space = vec![vec![vec![0; 300]; 10]; 10];
+    
+    for (index, brick) in bricks.iter().enumerate() {
+        brick.configure_space(&mut space, index as u16 +1);
+    }
 
-    let mut space = bricks.iter()
-        .enumerate()
-        .fold(
-        vec![vec![vec![0; 300]; 10]; 10],
-        |mut space, (index,brick)| {
-            for x in brick.start.0..=brick.end.0 {
-                for y in brick.start.1..=brick.end.1 {
-                    for z in brick.start.2..=brick.end.2 {
-                        space[x][y][z] = index + 1;
-                    }
-                }
-            }
-            space
-        }
-    );
+    bricks.sort_unstable_by_key(|b| b.start.2);
 
     drop_bricks(&mut bricks, &mut space);
 
@@ -120,25 +114,19 @@ pub fn solve(input: &str) -> Solution {
     let p1 = bricks.iter()
         .filter(|brick| {
             let above = brick.above(&space);            
-            above.len() == 0 || above.iter().all(|a| bricks[*a - 1].below(&space).len() > 1)
+            above.len() == 0 || above.iter().all(|a| bricks[*a as usize - 1].below(&space).len() > 1)
         })
         .count();
-    
+
     let p2: usize = bricks.iter().enumerate()
         .map(|(index, brick)| -> usize {
 
             let mut b0 = bricks.clone();
             let mut s2 = space.clone();
 
-            for x in brick.start.0..=brick.end.0 {
-                for y in brick.start.1..=brick.end.1 {
-                    for z in brick.start.2..=brick.end.2 {
-                        s2[x][y][z] = 0;
-                    }
-                }
-            }
-            b0.remove(index);
+            brick.configure_space(&mut s2, 0);
 
+            b0.remove(index);
             drop_bricks(&mut b0, &mut s2)
         })
         .sum();
@@ -152,8 +140,8 @@ impl FromStr for Brick {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         if let Some((p1, p2)) = s.split_once("~") {
 
-            let start = p1.split(',').take(3).map(|s| s.parse::<usize>().unwrap()).collect_tuple();
-            let end = p2.split(',').take(3).map(|s| s.parse::<usize>().unwrap()).collect_tuple();
+            let start = p1.split(',').take(3).map(|s| s.parse::<u16>().unwrap()).collect_tuple();
+            let end = p2.split(',').take(3).map(|s| s.parse::<u16>().unwrap()).collect_tuple();
             
             match (start, end) {
                 (Some(start),Some(end)) => Ok( Brick::initial(start, end) ),
