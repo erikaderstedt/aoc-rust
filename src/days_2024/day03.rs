335879e666ebb99856f8d2cd903b75ebf0bb99c0
Erik Aderstedt <erik@aderstedt.se>
2024-12-03 10:28:20 +0100

    Day 3 simplified

--- src/days_2024/day03.rs
+++ src/days_2024/day03.rs
@@ -5,6 +5,8 @@ use regex::Regex;
 
 pub fn solve(input: &str) -> Solution {
     let mul = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+    let stop_start = Regex::new(r"(?s)don't\(\).*?(do\(\)|$)").unwrap();
+
     let sum_in_range = |s: &str| -> usize {
         mul.captures_iter(s)
         .map(|m| m[2].parse::<usize>().unwrap() * m[1].parse::<usize>().unwrap())
@@ -12,23 +14,7 @@ pub fn solve(input: &str) -> Solution {
     };
 
     let p1 = sum_in_range(input);
-
-    let mut p2 = 0;
-    let mut offset = 0;
-    loop {
-        if let Some(end_here) = input[offset..].find(r"don't()") {
-            p2 += sum_in_range(&input[offset..(offset + end_here)]);
-            offset += end_here;
-        } else {
-            p2 += sum_in_range(&input[offset..]);
-            break;
-        }
-        if let Some(start_here) = input[offset..].find(r"do()") {
-            offset += start_here;
-        } else {
-            break;
-        }
-    }
+    let p2 = sum_in_range(&stop_start.replace_all(&input, " "));
 
     Solution::new(p1, p2)
 }
