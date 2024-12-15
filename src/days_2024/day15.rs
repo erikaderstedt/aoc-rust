c2ee2d3372bec81896e268daf5e7df1e45c86b2f
Erik Aderstedt <erik@aderstedt.se>
2024-12-15 15:55:05 +0100

    2024 day 15 optimized

--- src/days_2024/day15.rs
+++ src/days_2024/day15.rs
@@ -1,9 +1,5 @@
 // https://adventofcode.com/2024/day/15
 
-use std::collections::HashSet;
-
-use itertools::Itertools;
-
 use crate::{common::Solution, grid::{Direction, Grid, GridElement, Position}};
 
 #[derive(Debug,Clone,PartialEq,Eq)]
@@ -16,46 +12,7 @@ enum Warehouse {
     BoxRight,
 }
 
-fn normal_width(warehouse_s: &str, movements: &Vec<Direction>) -> usize {
-    let mut warehouse: Grid<Warehouse> = Grid::load(warehouse_s);
-
-    let mut p: Position = warehouse.find(&Warehouse::Robot).expect("No robot found!");
-
-    for m in movements.iter() {
-        let step = warehouse
-            .positions_along_direction(&p, m)
-            .take_while(|p| warehouse.get(p) == Some(Warehouse::Box))
-            .count() + 1;
-
-        let c = match m {
-            Direction::East => Position { row: p.row, column: p.column + step },
-            Direction::South => Position { row: p.row + step, column: p.column },
-            Direction::West => Position { row: p.row, column: p.column - step },
-            Direction::North => Position { row: p.row - step, column: p.column },
-        };
-
-        if warehouse.get(&c) == Some(Warehouse::Wall) {
-            continue;
-        }
-
-        assert!(warehouse.get(&c) == Some(Warehouse::Empty));
-        let v: Vec<Position> = warehouse.positions_along_direction(&p, m).take(step).collect(); 
-        for p1 in v.into_iter().rev() {
-            let n = warehouse.get(&p1.along(&m.reverse())).unwrap();
-            warehouse.set(&p1, n);
-        }
-        warehouse.set(&p, Warehouse::Empty);
-        p = p.along(m);
-    }
-
-    warehouse
-        .positions()
-        .filter(|p| warehouse.get(p) == Some(Warehouse::Box))
-        .map(|p| p.row * 100 + p.column).sum()
-}
-
-fn extra_width(warehouse_s: &str, movements: &Vec<Direction>) -> usize {
-    let mut warehouse: Grid<Warehouse> = Grid::load(warehouse_s);
+fn extra_width_warehouse(warehouse: &Grid<Warehouse>) -> Grid<Warehouse> {
     let mut l: Vec<Warehouse> = vec![Warehouse::Empty; warehouse.locations.len() * 2];
     for i in 0..warehouse.locations.len() {
         match warehouse.locations[i] {
@@ -65,93 +22,96 @@ fn extra_width(warehouse_s: &str, movements: &Vec<Direction>) -> usize {
             Warehouse::Empty | Warehouse::BoxLeft | Warehouse::BoxRight => {},
         };
     }
-    warehouse.locations = l;
-    warehouse.cols = warehouse.cols * 2;
+    Grid { rows: warehouse.rows, cols: warehouse.cols * 2, locations: l }
+}
+
+fn follow_movements<const PUSH_MULTIPLE: bool>(warehouse: &mut Grid<Warehouse>, movements: &Vec<Direction>) -> usize {
     let mut p: Position = warehouse.find(&Warehouse::Robot).expect("No robot found!");
-
+    warehouse.set(&p, Warehouse::Empty);
+    
     for m in movements.iter() {
-
-        if *m == Direction::North || *m == Direction::South {
-            // For each box we need to include a "chain" of indices to move 
+        if PUSH_MULTIPLE && (*m == Direction::North || *m == Direction::South) {
             let mut cols_to_check = vec![p.column];
             let mut row = p.row;
-            let mut stuck = false;
-            let mut can_move = false;
-            let mut positions_to_shift = vec![p.clone()];
-            while !stuck && !can_move {
+            let mut positions_to_shift = vec![];
+            loop {
                 row = if *m == Direction::North { row - 1} else { row + 1};
-
-                // If any cols to check find walls we skip to next movement
-                // If all cols to check find empty we move
-                // Otherwise add position to positions to shift
-                let content: Vec<Warehouse> = cols_to_check.iter().map(|c| warehouse.locations[row * warehouse.cols + c].clone()).collect();
-                if content.iter().contains(& Warehouse::Wall) {
-                    stuck = true;
-                    continue;
-                } else if content.iter().all(|c| *c == Warehouse::Empty) {
-                    can_move = true;
-                    continue;
-                }
-                cols_to_check = cols_to_check.into_iter().map(|c| {
-                    if warehouse.locations[row * warehouse.cols + c] == Warehouse::BoxLeft {
-                        vec![c, c+1].into_iter()
-                    } else if warehouse.locations[row*warehouse.cols + c] == Warehouse::BoxRight {
-                        vec![c-1, c].into_iter()
-                    } else {
-                        vec![].into_iter()
+                let mut next_cols_to_check: Vec<usize> = Vec::with_capacity(cols_to_check.len() * 2);
+                let mut found_wall = false;
+                for c in cols_to_check.into_iter() {
+                    match warehouse.locations[row * warehouse.cols + c] {
+                        Warehouse::BoxLeft => { next_cols_to_check.push(c); next_cols_to_check.push(c + 1); },
+                        Warehouse::BoxRight => { next_cols_to_check.push(c-1); next_cols_to_check.push(c); },
+                        Warehouse::Box => { next_cols_to_check.push(c); },
+                        Warehouse::Empty => {},
+                        Warehouse::Robot => { panic!("Should not find the robot while doing this."); }
+                        Warehouse::Wall => { found_wall = true; }
                     }
-                }).flatten().collect::<HashSet<usize>>().into_iter().collect();
-
-                positions_to_shift.extend(cols_to_check.iter().map(|c| Position { row: row.clone(), column: c.clone() }));
-            }
-            if !stuck {
-                for pq in positions_to_shift.into_iter().rev() {
-                    let na = warehouse.get(&pq).unwrap();
-                    let nb = warehouse.get(&pq.along(m)).unwrap();
-                    warehouse.set(&pq, nb);
-                    warehouse.set(&pq.along(m), na);
                 }
-                p = p.along(m);
+                if found_wall {
+                    break;
+                } else if next_cols_to_check.len() == 0 {
+                    for pq in positions_to_shift.iter().rev() {
+                        let na = warehouse.get(pq).unwrap();
+                        let nb = warehouse.get(&pq.along(m)).unwrap();
+                        warehouse.set(pq, nb);
+                        warehouse.set(&pq.along(m), na);
+                    }
+                    p = p.along(m);
+                    break;
+                } else {
+                    next_cols_to_check.sort_unstable();
+                    next_cols_to_check.dedup();
+                    cols_to_check = next_cols_to_check;
+                    positions_to_shift.extend(cols_to_check.iter().map(|c| Position { row: row.clone(), column: c.clone() }));
+                }
             }
         } else {
-            let step = warehouse
-            .positions_along_direction(&p, m)
-            .take_while(|p| warehouse.get(p) == Some(Warehouse::BoxLeft) || warehouse.get(p) == Some(Warehouse::BoxRight))
-            .count() + 1;
+            let boxes_in_a_row = warehouse
+                .positions_along_direction(&p, m)
+                .take_while(|p| match warehouse.get(p).unwrap() {
+                    Warehouse::BoxLeft | Warehouse::BoxRight | Warehouse::Box => true,
+                _ => false, })
+                .count();
 
-            let c = match m {
-                Direction::East => Position { row: p.row, column: p.column + step },
-                Direction::South => Position { row: p.row + step, column: p.column },
-                Direction::West => Position { row: p.row, column: p.column - step },
-                Direction::North => Position { row: p.row - step, column: p.column },
+            let last_box_position = match m {
+                Direction::East => Position { row: p.row, column: p.column + boxes_in_a_row },
+                Direction::South => Position { row: p.row + boxes_in_a_row, column: p.column },
+                Direction::West => Position { row: p.row, column: p.column - boxes_in_a_row },
+                Direction::North => Position { row: p.row - boxes_in_a_row, column: p.column },
             };
 
-            if warehouse.get(&c) == Some(Warehouse::Wall) {
-                continue;
+            if warehouse.get(&last_box_position.along(m)) == Some(Warehouse::Empty) {
+                let box_contents: Vec<Warehouse> = warehouse
+                .positions_along_direction(&p, m)
+                .take(boxes_in_a_row)
+                .map(|p| warehouse.get(&p).unwrap())
+                .collect(); 
+        
+                for (new_box_position, content) in warehouse
+                    .positions_along_direction(&p, m)
+                    .skip(1)
+                    .zip(box_contents.into_iter()) {
+                    warehouse.set(&new_box_position, content);
+                }
+                
+                p = p.along(m);
+                warehouse.set(&p, Warehouse::Empty);
             }
-
-            assert!(warehouse.get(&c) == Some(Warehouse::Empty));
-            let v: Vec<Position> = warehouse.positions_along_direction(&p, m).take(step).collect(); 
-            for p1 in v.into_iter().rev() {
-                let n = warehouse.get(&p1.along(&m.reverse())).unwrap();
-                warehouse.set(&p1, n);
-            }
-            warehouse.set(&p, Warehouse::Empty);
-            p = p.along(m);
         }
-
     }
-    // warehouse.display();
 
     warehouse
-    .positions()
-    .filter(|p| warehouse.get(p) == Some(Warehouse::BoxLeft))
-    .map(|p| p.row * 100 + p.column).sum()
+        .positions()
+        .filter(|p| warehouse.get(p) == Some(Warehouse::BoxLeft) || warehouse.get(p) == Some(Warehouse::Box))
+        .map(|p| p.row * 100 + p.column).sum()
 }
 
 pub fn solve(input: &str) -> Solution {
-
     let (warehouse_s, movements_s) = input.split_once("\n\n").unwrap();
+
+    let mut warehouse: Grid<Warehouse> = Grid::load(warehouse_s);
+    let mut extra_wide_warehouse = extra_width_warehouse(&warehouse);
     let movements: Vec<Direction> = movements_s.chars().filter_map(|c| -> Option<Direction> {
         match c {
         '^' => Some(Direction::North),
@@ -161,12 +121,11 @@ pub fn solve(input: &str) -> Solution {
         _ => None,
     }}).collect();
 
-    let p1 = normal_width(warehouse_s, &movements);
-    let p2 = extra_width(warehouse_s, &movements);
+    let p1 = follow_movements::<false>(&mut warehouse, &movements);
+    let p2 = follow_movements::<true>(&mut extra_wide_warehouse, &movements);
 
     Solution::new(p1, p2)
 }
-
 
 impl GridElement for Warehouse {
     fn from_char(c: &char) -> Option<Self> { 
