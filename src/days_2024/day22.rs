12fa91790b2eec706db3bc03cd2eb6788e7f55d0
Erik Aderstedt <erik@aderstedt.se>
2024-12-22 11:21:16 +0100

    2024 day 22 a bit faster

--- src/days_2024/day22.rs
+++ src/days_2024/day22.rs
@@ -11,62 +11,40 @@ fn evolve(i: &usize) -> usize {
     c
 }
 
+type Sequence = [i8;4];
+
 pub fn solve(input: &str) -> Solution {
     let seeds: Vec<usize> = input.lines().map(|line| line.trim().parse::<usize>().unwrap()).collect();
 
-    let p1 = seeds.iter().map(|seed| {
-        let mut x = seed.clone();
-        for _ in 0..2000 {
-            x = evolve(&x);
-        }
-        x
-    }).sum::<usize>();
+    let mut totals: HashMap<Sequence, isize> = HashMap::new(); 
+    let mut p1 = 0;
 
-    let data: Vec<HashMap<(i8,i8,i8,i8), i8>> = seeds.iter().map(|seed| {
+    for seed in seeds.iter() {
         let mut x = seed.clone();
-        let mut h: HashMap<(i8,i8,i8,i8), i8> = HashMap::new();
-        let mut prices = [0i8;2000]; 
+        let mut h: HashSet<Sequence> = HashSet::new();
+        let mut prices = [0i8;2000];
+
         for i in 0..2000 {
-            prices[i] = (x % 10) as i8;
-            if i >= 4 {
+            let current_price = (x % 10) as i8;
+            prices[i] = current_price;
+            if prices[i] != 0 && i >= 4 {
                 let d1 = prices[i] - prices[i-1];
                 let d2 = prices[i-1] - prices[i-2];
                 let d3 = prices[i-2] - prices[i-3];
                 let d4 = prices[i-3] - prices[i-4];
                 // a sequence of d4,d3,d2,d1 is worth what it is *the first time it is encountered*
-                let k = (d4,d3,d2,d1);
-                if !h.contains_key(&k) {
-                    h.insert(k, prices[i]);
+                let k = [d4,d3,d2,d1];
+                if !h.contains(&k) {
+                    h.insert(k);
+                    *totals.entry(k).or_default() += current_price as isize;
                 }
             }
             x = evolve(&x);
         }
-        h
-    }).collect();
+        p1 += x;
+    }
 
-    let all_diffs: HashSet<(i8,i8,i8,i8)> = data
-        .iter()
-        .map(|m| {
-            m.keys()
-        })
-        .flatten()
-        .cloned()
-        .collect();
-
-    println!("all diffs: {}", all_diffs.len());
-
-    let p2 = all_diffs
-        .iter()
-        .map(|diff| {
-            data
-             .iter()
-             .map(|m| 
-                *m
-                .get(diff)
-                .unwrap_or(&0) as isize
-            ).sum::<isize>()
-        })
-        .max().unwrap();
+    let p2 = totals.values().max().unwrap();
 
     Solution::new(p1, p2)
 }
