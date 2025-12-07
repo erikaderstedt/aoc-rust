94d4a48eb0285edd1f2b44616802a5a6fcf9dc7c
Erik Aderstedt <erik@aderstedt.se>
2025-12-07 20:03:01 +0100

    Use vec instead of hashmap in day 7

--- src/days_2025/day07.rs
+++ src/days_2025/day07.rs
@@ -1,39 +1,31 @@
 // https://adventofcode.com/2025/day/7
 
-use std::collections::HashMap;
 use crate::common::Solution;
 
-fn update(m: &mut HashMap<usize,usize>, index: usize, n: usize) {
-    if let Some(x) = m.get_mut(&index) {
-        *x = *x + n
-    } else {
-        m.insert(index, n);
-    }
-}
-
 pub fn solve(input: &str) -> Solution {
-    let mut beams: HashMap<usize, usize> = HashMap::new();
-    
-    beams.insert(input.lines().next().unwrap().find('S').unwrap(), 1);
+    let first_line = input.lines().next().unwrap();
+    let start_position = first_line.find('S').unwrap();
+    let n = first_line.len();
+
+    let mut beams = vec![0; n];
+    beams[start_position] = 1;
 
     let mut p1 = 0;
     for row in input.lines().skip(2).step_by(2) {
-        let mut b: HashMap<usize, usize> = HashMap::new();
-        for (i, n) in beams.iter() {
-            if row.as_bytes()[*i] == b'^' {
-                update(&mut b, i-1, *n);
-                update(&mut b, i+1, *n);
+        let mut b2 = vec![0; n];
+        for (i, n) in beams.iter().enumerate() {
+            if *n == 0 { continue; }
+            if row.as_bytes()[i] == b'^' {
+                b2[i-1] += n;
+                b2[i+1] += n;
                 p1 = p1 + 1;
             } else {
-                update(&mut b, *i, *n);
+                b2[i] += n;
             }
         }
-        beams = b;
+        beams = b2;
     }
-    let p2: usize = beams
-        .iter()
-        .map(|(_, v)| v)
-        .sum::<usize>();
+    let p2: usize = beams.iter().sum::<usize>();
 
     Solution::new(p1, p2)
 }
