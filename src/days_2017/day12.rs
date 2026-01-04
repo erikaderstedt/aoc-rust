e1f2230a0d6666cf1ad3d1e31ae96f821b6d431e
Erik Aderstedt <erik@aderstedt.se>
2026-01-04 21:31:08 +0100

    2017 days 12 and 13

--- src/days_2017/day12.rs
+++ src/days_2017/day12.rs
@@ -1,63 +1,49 @@
 // https://adventofcode.com/2017/day/12
 
-use std::collections::HashSet;
-use itertools::Itertools;
 use crate::common::Solution;
+use itertools::Itertools;
 
 pub fn solve(input: &str) -> Solution {
-    let data: Vec<Vec<usize>> = input
+    let mut data: Vec<(usize, Vec<usize>)> = input
         .lines()
         .enumerate()
         .map(|(i, line)| {
             let (left_program, programs) = line.split(" <-> ").collect_tuple().unwrap();
             let left = left_program.parse::<usize>().unwrap();
             assert!(left == i);
-            programs
-                .split(", ")
-                .map(|s| s.parse::<usize>().unwrap())
-                .collect()
+            (
+                left,
+                programs
+                    .split(", ")
+                    .map(|s| s.parse::<usize>().unwrap())
+                    .collect(),
+            )
         })
         .collect();
 
-    let mut groups: Vec<usize> = (0..data.len());
-    groups[0] = Some(0);
+    let mut p1 = 0;
+    let mut p2 = 0;
+    while let Some(start_point) = data.pop() {
+        let mut group_contains_zero = false;
+        let mut investigate = vec![start_point];
+        let mut n = 0;
+        while let Some(p) = investigate.pop() {
+            n = n + 1;
+            if p.0 == 0 {
+                group_contains_zero = true;
+            }
 
-    for (i, p) in data.iter().enumerate() {
-        let group_id = p.iter()
-            .cloned()
-            .chain(vec![i])
-            .filter_map(|n| groups[n])
-            .min()
-            .unwrap_or(i);
-
-        // For each group id in these, set 
-
-        // If any is part of a group, 
-
-        // set the 
-    }
-
-    for line in input.lines() {
-        let (left_program, programs) = line.split(" <-> ").collect_tuple().unwrap();
-        let left = left_program.parse::<usize>().unwrap();
-        let programs: Vec<usize> = programs
-            .split(", ")
-            .map(|s| s.parse::<usize>().unwrap())
-            .collect();
-        if let Some(group_index) = groups.iter().position(|group| {
-            group.contains(&left) || programs.iter().any(|p| group.contains(p))
-        }) {
-            groups[group_index].insert(left);
-            groups[group_index].extend(programs.into_iter());
-        } else {
-            let mut new_group = HashSet::new();
-            new_group.insert(left);
-            new_group.extend(programs.into_iter());
-            groups.push(new_group);
+            for program in p.1 {
+                if let Some(i) = data.iter().position(|q| q.0 == program) {
+                    investigate.push(data.remove(i));
+                }
+            }
         }
-    }
-    let p1 = groups.iter().find(|g| g.contains(&0)).unwrap().len();
-    let p2 = groups.len();
 
-    Solution::new(p1,p2)
+        if group_contains_zero {
+            p1 = n;
+        }
+        p2 += 1;
+    }
+    Solution::new(p1, p2)
 }
--- src/days_2017/day13.rs
+++ src/days_2017/day13.rs
@@ -1,11 +1,34 @@
-// https://adventofcode.com/2078/day/1
+// https://adventofcode.com/2017/day/13
 
 use crate::common::Solution;
+use itertools::Itertools;
 
-pub fn solve(_input: &str) -> Solution {
+pub fn solve(input: &str) -> Solution {
+    let mut scanners: Vec<(usize, usize, usize)> = vec![];
 
-    let p1 = 0;
-    let p2 = 0;
+    for line in input.lines() {
+        let (depth, range) = line
+            .split(": ")
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect_tuple()
+            .unwrap();
+        let period = 2 * (range - 1);
+        scanners.push((depth, range, period));
+    }
 
-    Solution::new(p1,p2)
+    let p1 = scanners
+        .iter()
+        .filter(|(depth, _, period)| depth % period == 0)
+        .map(|(depth, range, _)| depth * range)
+        .sum::<usize>();
+
+    let p2 = (0..)
+        .find(|delay| {
+            scanners
+                .iter()
+                .all(|(depth, _, period)| (delay + depth) % period != 0)
+        })
+        .unwrap();
+
+    Solution::new(p1, p2)
 }
