d20099f883bfe8b69ba0a16b708ccdf25aa8e713
Erik Aderstedt <erik@aderstedt.se>
2026-01-04 22:08:21 +0100

    2017 day 14

--- src/days_2017/day10.rs
+++ src/days_2017/day10.rs
@@ -1,20 +1,21 @@
 // https://adventofcode.com/2017/day/10
 
-use crate::common::Solution;
 use itertools::Itertools;
 
-fn round<const N: usize>(lengths: &Vec<usize>) -> Vec<usize> {
+use crate::common::Solution;
+
+fn round<const N: usize>(lengths: &Vec<u8>) -> Vec<u8> {
     let mut pos = 0;
     let mut skip_size = 0;
-    let mut n: Vec<usize> = (0..256).collect();
+    let mut n: Vec<u8> = (0..=255).collect();
     for _ in 0..N {
         for length in lengths.iter() {
             for i in 0..(length / 2) {
                 let src = (pos + i) & 255;
                 let dst = (pos + (length - 1 - i)) & 255;
-                let v = n[dst];
-                n[dst] = n[src];
-                n[src] = v;
+                let v = n[dst as usize];
+                n[dst as usize] = n[src as usize];
+                n[src as usize] = v;
             }
             pos = (pos + length + skip_size) & 255;
             skip_size += 1;
@@ -24,33 +25,35 @@ fn round<const N: usize>(lengths: &Vec<usize>) -> Vec<usize> {
     n
 }
 
+pub fn knot_hash(s: &str) -> [u8; 16] {
+    let lengths = s
+        .as_bytes()
+        .iter()
+        .cloned()
+        .chain(vec![17, 31, 73, 47, 23])
+        .collect();
+    round::<64>(&lengths)
+        .chunks(16)
+        .map(|c| c.iter().cloned().reduce(|a, b| a ^ b).unwrap())
+        .collect_array()
+        .unwrap()
+}
+
 pub fn solve(input: &str) -> Solution {
     let p1 = {
         let lengths = input
             .trim()
             .split(',')
-            .map(|s| s.parse::<usize>().unwrap())
+            .map(|s| s.parse::<u8>().unwrap())
             .collect();
         let v = round::<1>(&lengths);
-        v[0] * v[1]
+        (v[0] as usize) * (v[1] as usize)
     };
 
-    let p2 = {
-        let lengths = input
-            .trim()
-            .as_bytes()
-            .iter()
-            .map(|b| *b as usize)
-            .chain(vec![17, 31, 73, 47, 23])
-            .collect();
-        round::<64>(&lengths)
-            .chunks(16)
-            .map(|c| {
-                let value = c.iter().cloned().reduce(|a, b| a ^ b).unwrap();
-                format!("{:02x}", value)
-            })
-            .join("")
-    };
+    let p2 = knot_hash(input.trim())
+        .iter()
+        .map(|c| format!("{:02x}", c))
+        .join("");
 
     Solution::new(p1, p2)
 }
--- src/days_2017/day14.rs
+++ src/days_2017/day14.rs
@@ -1,11 +1,46 @@
-// https://adventofcode.com/2078/day/1
+// https://adventofcode.com/2017/day/14
 
 use crate::common::Solution;
+use crate::days_2017::day10::knot_hash;
 
-pub fn solve(_input: &str) -> Solution {
+pub fn solve(input: &str) -> Solution {
+    let mut grid = vec![0u8; 128 * 128];
+    for row in 0..128 {
+        let s = format!("{}-{}", input.trim(), row);
+        for (i, h) in knot_hash(&s).iter().enumerate() {
+            for k in 0..8 {
+                grid[row * 128 + (i * 8 + 7 - k)] = (1 << k) & h;
+            }
+        }
+    }
+    let p1 = grid.iter().filter(|v| **v != 0).count();
 
-    let p1 = 0;
-    let p2 = 0;
+    let mut p2 = 0;
+    while let Some(non_zero_index) = grid.iter().position(|g| *g != 0) {
+        grid[non_zero_index] = 0;
+        let mut search = vec![non_zero_index];
 
-    Solution::new(p1,p2)
+        while let Some(idx) = search.pop() {
+            // Check neighbors and add to search if != 0
+            if idx >= 128 && grid[idx - 128] != 0 {
+                grid[idx - 128] = 0;
+                search.push(idx - 128);
+            }
+            if idx % 128 != 0 && grid[idx - 1] != 0 {
+                grid[idx - 1] = 0;
+                search.push(idx - 1);
+            }
+            if idx % 128 != 127 && grid[idx + 1] != 0 {
+                grid[idx + 1] = 0;
+                search.push(idx + 1);
+            }
+            if idx < grid.len() - 128 && grid[idx + 128] != 0 {
+                grid[idx + 128] = 0;
+                search.push(idx + 128);
+            }
+        }
+        p2 += 1;
+    }
+
+    Solution::new(p1, p2)
 }
