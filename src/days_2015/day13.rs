beb14bb7836c92786afe51a0a43115a4f224973c
Erik Aderstedt <erik@aderstedt.se>
2026-01-07 10:40:36 +0100

    2015 days 13 and 14

--- src/days_2015/day13.rs
+++ src/days_2015/day13.rs
@@ -1,10 +1,53 @@
 // https://adventofcode.com/2015/day/13
 
 use crate::common::Solution;
+use itertools::Itertools;
 
-pub fn solve(_input: &str) -> Solution {
-    let p1 = 0;
-    let p2 = 0;
+fn find_optimal(deltas: &Vec<Vec<i64>>) -> i64 {
+    let n = deltas.len();
+    (0..n)
+        .permutations(n)
+        .map(|i| {
+            i.into_iter()
+                .circular_tuple_windows()
+                .map(|(a, b)| deltas[a][b] + deltas[b][a])
+                .sum::<i64>()
+        })
+        .max()
+        .unwrap()
+}
+
+pub fn solve(input: &str) -> Solution {
+    let relations: Vec<(&str, i64, &str)> = input
+        .lines()
+        .map(|line| {
+            let (subject, _, gain_or_lose, units, _, _, _, _, _, _, object) = line
+                [0..(line.len() - 1)]
+                .split(' ')
+                .collect_tuple()
+                .unwrap();
+            let delta = units.parse::<i64>().unwrap() * if gain_or_lose == "gain" { 1 } else { -1 };
+            (subject, delta, object)
+        })
+        .collect();
+
+    let names: Vec<&str> = relations.iter().map(|r| r.0).dedup().collect();
+    let n = names.len();
+    let mut deltas: Vec<Vec<i64>> = vec![vec![0; n]; n];
+    for r in relations.iter() {
+        let i = names.iter().position(|p| *p == r.0).unwrap();
+        let j = names.iter().position(|p| *p == r.2).unwrap();
+        deltas[i][j] = r.1;
+    }
+
+    let p1 = find_optimal(&deltas);
+
+    for i in deltas.iter_mut() {
+        i.push(0);
+    }
+    deltas.push(vec![0; n + 1]);
+
+    let p2 = find_optimal(&deltas);
 
     Solution::new(p1, p2)
 }
--- src/days_2015/day14.rs
+++ src/days_2015/day14.rs
@@ -1,11 +1,56 @@
-// https://adventofcode.com/2015/day/11
+// https://adventofcode.com/2015/day/14
 
 use crate::common::Solution;
+use regex::Regex;
 
-pub fn solve(_input: &str) -> Solution {
+struct Reindeer {
+    speed: usize,
+    duration: usize,
+    rest: usize,
 
-    let p1 = 0;
-    let p2 = 0;
+    score: usize,
+    distance: usize,
+}
+
+const TIME: usize = 2503;
+
+pub fn solve(input: &str) -> Solution {
+    let r = Regex::new(
+        "^\\w+ can fly (\\d+) km/s for (\\d+) seconds, but then must rest for (\\d+) seconds.",
+    )
+    .unwrap();
+
+    let mut reindeer: Vec<Reindeer> = input
+        .lines()
+        .map(|line| {
+            let (_, [speed, duration, rest]) = r.captures(line).unwrap().extract();
+            Reindeer {
+                speed: speed.parse::<usize>().unwrap(),
+                duration: duration.parse::<usize>().unwrap(),
+                rest: rest.parse::<usize>().unwrap(),
+                score: 0,
+                distance: 0,
+            }
+        })
+        .collect();
+
+    for t in 0..TIME {
+        for r in reindeer.iter_mut() {
+            let period = r.duration + r.rest;
+            if t % period < r.duration {
+                r.distance += r.speed;
+            }
+        }
+        // Check which one is in front and increase the score.
+        // For ties each get a point.
+        let furthest = reindeer.iter().map(|r| r.distance).max().unwrap();
+        for r in reindeer.iter_mut().filter(|r| r.distance == furthest) {
+            r.score += 1;
+        }
+    }
+
+    let p1 = reindeer.iter().map(|r| r.distance).max().unwrap();
+    let p2 = reindeer.iter().map(|r| r.score).max().unwrap();
 
     Solution::new(p1, p2)
 }
